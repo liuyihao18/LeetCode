8a5a37815b07f2c63678e497b4d3fcc40a1c1083
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-09-20 10:55:37 +0800

    add 673

---
+++ LeetCode/673_最长递增子序列的个数.cpp
@@ -0,0 +1,38 @@
+// 673_最长递增子序列的个数.cpp
+#include "custom.h"
+#include <iterator>
+#include <iostream>
+#include <map>
+ustd
+
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        vector<size_t> length(nums.size(), 1);
+        vector<size_t> counts(nums.size(), 1);
+        size_t max_length{ 1 };
+        size_t max_count{ 1 };
+        for (size_t i = 1; i < nums.size(); i++) {
+            for (size_t j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    if (length[i] < length[j] + 1) {
+                        length[i] = length[j] + 1;
+                        counts[i] = counts[j];
+                    }
+                    else if (length[i] == length[j] + 1) {
+                        length[i] = length[j] + 1;
+                        counts[i] += counts[j];
+                    }
+                }
+            }
+            if (length[i] > max_length) {
+                max_length = length[i];
+                max_count = counts[i];
+            }
+            else if (length[i] == max_length) {
+                max_count += counts[i];
+            }
+        }
+        return max_count;
+    }
+};
