4cd5315c9e1232d439431831e8c1ddf7fd8de198
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-09-22 10:58:47 +0800

    add 725

---
+++ LeetCode/725_分隔链表.cpp
@@ -0,0 +1,41 @@
+// 725_∑÷∏Ù¡¥±Ì.cpp
+#include "custom.h"
+ustd
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        int length = 0;
+        auto p = head;
+        while (p) {
+            length++;
+            p = p->next;
+        }
+        int avg_length = length / k;
+        int res = length - avg_length * k;
+        vector<ListNode*> result;
+        ListNode* pred = nullptr;
+        p = head;
+        for (int i = 0; i < k; i++) {
+            result.push_back(p);
+            int go_next = i < res ? avg_length + 1 : avg_length;
+            while (go_next > 0) {
+                go_next--;
+                if (p) {
+                    pred = p; 
+                    p = p->next;
+                }
+            }
+            if (pred) pred->next = nullptr;
+        }
+        return result;
+    }
+};
