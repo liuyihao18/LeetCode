f755dff5993732730228b6fe4c3838cd1d7a0ee4
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-10-02 09:13:17 +0800

    add 405

---
+++ LeetCode/1436_旅行终点站.cpp
@@ -0,0 +1,19 @@
+// 1436_旅行终点站.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        map<string, string> src_to_dest;
+        for (const auto& a_path : paths) {
+            src_to_dest[a_path[0]/* src */] = a_path[1] /* dest */;
+        }
+        for (auto iter = src_to_dest.begin(); iter != src_to_dest.end(); ++iter) {
+            if (src_to_dest.find(iter->second) == src_to_dest.end()) {
+                return iter->second;
+            }
+        }
+        return ""; // 不会遇到
+    }
+};
---
+++ LeetCode/405_数字转换为十六进制数.cpp
@@ -0,0 +1,54 @@
+// 405_数字转换为十六进制数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    string toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        if (num == -2147483648) {
+            return "80000000";
+        }
+        bool negative = num < 0;
+        num = negative ? -num : num;
+        string result;
+        const char remainder2hex[] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
+        while (num) {
+            int remainder = num % 16;
+            if (negative) remainder = 15 - remainder;
+            result.push_back(remainder2hex[remainder]);
+            num /= 16;
+        }
+        reverse(result.begin(), result.end());
+        if (negative) {
+            ++*result.rbegin();
+            if (*result.rbegin() == '9' + 1) {
+                *result.rbegin() = 'a';
+            }
+            bool carry = false;
+            for (auto iter = result.rbegin(); iter != result.rend(); ++iter) {
+                if (carry) {
+                    ++*iter;
+                    if (*iter == '9' + 1) {
+                        *iter = 'a';
+                    }
+                    carry = false;
+                }
+                if (*iter == 'f' + 1) {
+                    carry = true;
+                    *iter = '0';
+                }
+            }
+            if (carry) {
+                result.insert(result.begin(), '1');
+                carry = false;
+            }
+            while (result.size() != 8) {
+                result.insert(result.begin(), 'f');
+            }
+        }
+        return result;
+    }
+};
