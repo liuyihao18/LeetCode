7ebf25223aa1382940c7f749e34ef5d030e98201
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-10-05 13:45:14 +0800

    add 284

---
+++ LeetCode/284_窥探迭代器.cpp
@@ -0,0 +1,55 @@
+// 284_¿úÌ½µü´úÆ÷.cpp
+#include "custom.h"
+ustd
+
+// Below is the interface for Iterator, which is already defined for you.
+// **DO NOT** modify the interface for Iterator.
+class Iterator {
+	struct Data;
+	Data* data;
+public:
+	Iterator(const vector<int>& nums);
+	Iterator(const Iterator& iter);
+
+	// Returns the next element in the iteration.
+	int next();
+
+	// Returns true if the iteration has more elements.
+	bool hasNext() const;
+};
+
+class PeekingIterator : public Iterator {
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+		// Initialize any member here.
+		// **DO NOT** save a copy of nums and manipulate it directly.
+		// You should only use the Iterator interface methods.
+		flag = Iterator::hasNext();
+		if (flag) {
+			nextElement = Iterator::next();
+		}
+	}
+
+	// Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+		return nextElement;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+		int temp = nextElement;
+		flag = Iterator::hasNext();
+		if (flag) {
+			nextElement = Iterator::next();
+		}
+		return temp;
+	}
+
+	bool hasNext() const {
+		return flag;
+	}
+private:
+	int nextElement{ 0 };
+	bool flag{ false };
+};
