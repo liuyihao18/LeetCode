65fcd478217543991d7a558cfe3436913b7ff48f
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-10-07 17:13:42 +0800

    add 434

---
+++ LeetCode/434_字符串中的单词数.cpp
@@ -0,0 +1,16 @@
+// 434_字符串中的单词数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int countSegments(string s) {
+        stringstream in(s);
+        string word;
+        size_t count{ 0 };
+        while (in >> word) {
+            ++count;
+        }
+        return count;
+    }
+};
