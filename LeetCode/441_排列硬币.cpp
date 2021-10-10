169957457e9bcbecece7cd1689d1bba30627b128
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-10-10 10:05:20 +0800

    add 441

---
+++ LeetCode/441_排列硬币.cpp
@@ -0,0 +1,11 @@
+// 441_≈≈¡–”≤±“.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long delta = static_cast<long long>(sqrt(1L + 8L * n));
+        return (delta - 1) / 2;
+    }
+};
