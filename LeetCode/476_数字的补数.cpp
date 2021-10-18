fd9ffa82a4ed5db39ea688f21f1df0c28f288350
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-10-18 13:36:15 +0800

    add 476

---
+++ LeetCode/476_数字的补数.cpp
@@ -0,0 +1,21 @@
+// 476_Êı×ÖµÄ²¹Êı.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int findComplement(int num) {
+        int highest_bit = 31;
+        for (; highest_bit >= 0; highest_bit--) {
+            if (num & (1 << highest_bit)) {
+                break;
+            }
+        }
+        int mask = 1;
+        for (int i = 0; i < highest_bit; i++) {
+            mask <<= 1;
+            mask |= 1;
+        }
+        return ~num & mask;
+    }
+};
