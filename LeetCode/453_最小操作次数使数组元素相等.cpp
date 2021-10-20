f57b5a8aa0c7c6b4d269a15e626d365c9ba62895
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-10-20 09:33:24 +0800

    add 453

---
+++ LeetCode/453_最小操作次数使数组元素相等.cpp
@@ -0,0 +1,15 @@
+// 453_最小操作次数使数组元素相等.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int min_num = *min_element(nums.begin(), nums.end());
+        int result = 0;
+        for (auto num : nums) {
+            result += num - min_num;
+        }
+        return result;
+    }
+};
