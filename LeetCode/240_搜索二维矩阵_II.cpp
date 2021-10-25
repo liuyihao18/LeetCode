9f849e359f3783bac78c4457cbfccdf503b08591
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-10-25 14:51:04 +0800

    update 240

--- LeetCode/240_搜索二维矩阵_II.cpp
+++ LeetCode/240_搜索二维矩阵_II.cpp
@@ -1,7 +1,6 @@
 // 240_ËÑË÷¶þÎ¬¾ØÕó_II.cpp
 #include "custom.h"
 ustd
-#define BINARY
 
 class Solution {
 public:
@@ -43,6 +42,21 @@ public:
     }
 #else 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        pair<size_t, size_t> BST_root = { 0, matrix[0].size() - 1 };
+        auto isValid = [&](const pair<size_t, size_t> root) {
+            return root.first < matrix.size() && root.second != -1;
+        };
+        while (isValid(BST_root)) {
+            if (matrix[BST_root.first][BST_root.second] == target) {
+                return true;
+            }
+            if (matrix[BST_root.first][BST_root.second] > target) {
+                BST_root.second--;  // ×ó×ÓÊ÷
+            }
+            else {
+                BST_root.first++;  // ÓÒ×ÓÊ÷
+            }
+        }
         return false;
     }
 #endif
