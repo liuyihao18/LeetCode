3237edf19aac1f008f92428ba34a66b610a3451a
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-10-29 23:37:07 +0800

    add 335

--- LeetCode/335_路径交叉.cpp
+++ LeetCode/335_路径交叉.cpp
@@ -4,28 +4,34 @@ ustd
 
 class Solution {
 public:
-    bool isSelfCrossing(vector<int>& distance) {
-        int n = distance.size();
-        for (int i = 3; i < n; ++i) {
-            // 第 1 类路径交叉的情况
-            if (distance[i] >= distance[i - 2] && distance[i - 1] <= distance[i - 3]) {
-                return true;
-            }
-
-            // 第 2 类路径交叉的情况
-            if (i == 4 && (distance[3] == distance[1]
-                && distance[4] >= distance[2] - distance[0])) {
-                return true;
-            }
-
-            // 第 3 类路径交叉的情况
-            if (i >= 5 && (distance[i - 3] - distance[i - 5] <= distance[i - 1]
-                && distance[i - 1] <= distance[i - 3]
-                && distance[i] >= distance[i - 2] - distance[i - 4]
-                && distance[i - 2] > distance[i - 4])) {
-                return true;
-            }
+    bool isSelfCrossing(vector<int>& x) {
+        for (int i = 3, l = x.size(); i < l; i++) {
+            // Case 1: current line crosses the line 3 steps ahead of it
+            if (x[i] >= x[i - 2] && x[i - 1] <= x[i - 3]) return true;
+            // Case 2: current line crosses the line 4 steps ahead of it
+            else if (i >= 4 && x[i - 1] == x[i - 3] && x[i] + x[i - 4] >= x[i - 2]) return true;
+            // Case 3: current line crosses the line 6 steps ahead of it
+            else if (i >= 5 && x[i - 2] >= x[i - 4] && x[i] + x[i - 4] >= x[i - 2] && x[i - 1] <= x[i - 3] && x[i - 1] + x[i - 5] >= x[i - 3]) return true;
         }
         return false;
     }
+
+    /*               i-2
+        case 1 : i-1┌─┐
+                    └─┼─>i
+                     i-3
+
+                        i-2
+        case 2 : i-1 ┌────┐
+                     └─══>┘i-3
+                     i  i-4      (i overlapped i-4)
+
+        case 3 :    i-4
+                   ┌──┐
+                   │i<┼─┐
+                i-3│ i-5│i-1
+                   └────┘
+                    i-2
+
+    */
 };
