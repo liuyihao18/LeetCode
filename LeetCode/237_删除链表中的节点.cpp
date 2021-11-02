cbbaeb875536ae279e48865130faf77166777b2c
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-11-02 15:17:36 +0800

    add 237

---
+++ LeetCode/237_删除链表中的节点.cpp
@@ -0,0 +1,20 @@
+// 237_删除链表中的节点.cpp
+#include "custom.h"
+ustd
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        ListNode* next = node->next;
+        node->next = node->next->next;
+        delete next; // 最好删除一下节点？
+    }
+};
