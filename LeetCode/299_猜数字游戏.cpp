b1ec6dae5f584239862d11a0a179a1f44a7c25b8
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-11-08 14:54:37 +0800

    add 299

---
+++ LeetCode/299_猜数字游戏.cpp
@@ -0,0 +1,26 @@
+// 299_²ÂÊı×ÖÓÎÏ·.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        size_t x = 0;
+        size_t y = 0;
+        vector<size_t> secret_count(10, 0);
+        vector<size_t> guess_count(10, 0);
+        for (size_t i = 0; i < secret.size(); i++) {
+            if (secret[i] == guess[i]) {
+                x++;
+            }
+            else {
+                secret_count[secret[i] - '0']++;
+                guess_count[guess[i] - '0']++;
+            }
+        }
+        for (size_t i = 0; i < secret_count.size(); i++) {
+            y += min(secret_count[i], guess_count[i]);
+        }
+        return to_string(x) + 'A' + to_string(y) + 'B';
+    }
+};
