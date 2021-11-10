4297c1cd37e9cc560ffb6ee65835d760ca4910f9
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-11-10 10:45:19 +0800

    add 495

---
+++ LeetCode/495_提莫攻击.cpp
@@ -0,0 +1,24 @@
+// 495_ÌáÄª¹¥»÷.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int result = 0;
+        int begin_time = -1;
+        int end_time = -1;
+        for (auto time : timeSeries) {
+            if (time > end_time) {
+                result += end_time - begin_time;
+                begin_time = time;
+                end_time = time + duration;
+            }
+            else {
+                end_time = time + duration;
+            }
+        }
+        result += end_time - begin_time;
+        return result;
+    }
+};
