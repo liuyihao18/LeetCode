d4d2fd93b4e6a40fa74f09e65b9eee882aee874a
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-11-17 10:17:40 +0800

    add 318

---
+++ LeetCode/318_最大单词长度乘积.cpp
@@ -0,0 +1,33 @@
+// 318_最大单词长度乘积.cpp
+#include "custom.h"
+ustd
+#define HASH
+
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+#ifdef HASH
+        vector<size_t> hash_table;
+        for (auto word : words) {
+            size_t s = 0;
+            for (auto c : word) {
+                s |= (static_cast<size_t>(1) << (c - 'a'));
+            }
+            hash_table.push_back(s);
+        }
+#endif // HASH
+        size_t max_length_product = 0;
+        for (size_t i = 0; i < words.size(); i++) {
+            for (size_t j = i + 1; j < words.size(); j++) {
+#ifdef HASH
+                if ((hash_table[i] & hash_table[j]) == 0) {
+#else
+                if (words[i].find_first_of(words[j]) == string::npos) {
+#endif // HASH
+                    max_length_product = max(max_length_product, words[i].size() * words[j].size());
+                }
+            }
+        }
+        return static_cast<int>(max_length_product);
+    }
+};
