255f229cfea4227f76fe3b9400102b4c2528c642
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-11-21 10:04:24 +0800

    add 559

--- LeetCode/559_N叉树的最大深度.cpp
+++ LeetCode/559_N叉树的最大深度.cpp
@@ -1,6 +1,7 @@
 // 559_N叉树的最大深度.cpp
 #include "custom.h"
 ustd
+#define STACK
 
 // Definition for a Node.
 class Node {
@@ -23,6 +24,7 @@ public:
 class Solution {
 public:
     int maxDepth(Node* root) {
+#ifdef DFS
         if (!root) {
             return 0;
         }
@@ -31,5 +33,24 @@ public:
             max_depth = max(max_depth, maxDepth(child) + 1);
         }
         return max_depth;
+#endif // STACK
+#ifdef STACK
+        if (!root) {
+            return 0;
+        }
+        int max_height = 1;
+        stack<pair<Node*, int>> s;
+        s.push({ root,1 });
+        while (!s.empty()) {
+            auto parent = s.top();
+            s.pop();
+            max_height = max(max_height, parent.second);
+            for (auto child : parent.first->children) {
+                s.push({ child, parent.second + 1 });
+            }
+        }
+        return max_height;
+#endif // STACK
+
     }
 };
