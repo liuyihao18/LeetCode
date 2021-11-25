ce7fa5ab782130ec4780569642fb50dcfd9ec326
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-11-25 13:47:51 +0800

    add 458

---
+++ LeetCode/458_可怜的小猪.cpp
@@ -0,0 +1,15 @@
+// 458_可怜的小猪.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        // 一只小猪最多喝T次水，则其携带信息量为(T+1)
+        // x只小猪最多携带(T+1)^x的信息量
+        // 或者认为是x维空间最多有(T+1)^x个点，其中每个点由x维坐标确定
+        int T = minutesToTest / minutesToDie;
+        int x = static_cast<int>(ceil(log(buckets) / log(T + 1)));
+        return x;
+    }
+};
