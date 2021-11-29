39d4258c1707c142c06fee731d38c95a2d44c1db
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-11-29 14:05:29 +0800

    add 786

---
+++ LeetCode/786_第K个最小的素数分数.cpp
@@ -0,0 +1,25 @@
+// 786_第K个最小的素数分数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    struct Node {
+        int arr_i;
+        int arr_j;
+        bool operator<(const Node& n) const {
+            return arr_i * n.arr_j < n.arr_i * arr_j;
+        }
+    };
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        set<Node> nodes;
+        for (size_t i = 0; i < arr.size() - 1; i++) {
+            for (size_t j = i + 1; j < arr.size(); j++) {
+                nodes.insert({ arr[i], arr[j] });
+            }
+        }
+        auto iter = nodes.begin();
+        for (size_t i = 1; i < static_cast<size_t>(k); i++, ++iter);
+        return { iter->arr_i, iter->arr_j };
+    }
+};
