22d2c1e91b276e3a9513b18ed4ee52b70a6fb697
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-12-01 09:50:15 +0800

    add 1446

---
+++ LeetCode/1446_连续字符.cpp
@@ -0,0 +1,27 @@
+// 1446_Á¬Ðø×Ö·û.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maxPower(const string& s) {
+        if (s.size() == 0) {
+            return 0;
+        }
+        int max_length = 0;
+        int length = 0;
+        string::value_type last = -1;
+        for (auto c : s) {
+            if (c == last) {
+                length++;
+            }
+            else {
+                max_length = max(max_length, length);
+                last = c;
+                length = 1;
+            }
+        }
+        max_length = max(max_length, length);
+        return max_length;
+    }
+};
