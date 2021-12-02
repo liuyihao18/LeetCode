7f04b93669b90079561f85444b917bc93e2613ed
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-12-02 13:50:16 +0800

    add 506

---
+++ LeetCode/506_相对名次.cpp
@@ -0,0 +1,32 @@
+// 506_相对名次.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    struct Athlete {
+        size_t index;
+        int score;
+        bool operator>(const Athlete& a) const {
+            return score > a.score;
+        }
+    };
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        vector<Athlete> athletes;
+        for (size_t i = 0; i < score.size(); i++) {
+            athletes.push_back({ i, score[i] });
+        }
+        sort(athletes.begin(), athletes.end(), greater<Athlete>());
+        vector<string> result(athletes.size());
+        vector<string> _map = { "Gold Medal","Silver Medal","Bronze Medal" };
+        for (size_t i = 0; i < athletes.size(); i++) {
+            if (i < 3) {
+                result[athletes[i].index] = _map[i];
+            }
+            else {
+                result[athletes[i].index] = to_string(i + 1);
+            }
+        }
+        return result;
+    }
+};
