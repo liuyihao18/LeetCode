ec4f3d8c2734a967630d7dacd21af201dba7a6f1
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-12-03 17:29:47 +0800

    add 1005

---
+++ LeetCode/1005_K次取反后最大化的数组和.cpp
@@ -0,0 +1,28 @@
+// 1005_K次取反后最大化的数组和.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+        if (nums.empty()) {
+            return 0;
+        }
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        int boarder = abs(nums[0]);
+        for (auto num : nums) {
+            boarder = min(abs(num), boarder);
+            if (num <= 0 && k-- > 0) {
+                sum -= num;
+            }
+            else {
+                sum += num;
+            }
+        }
+        if (k > 0 && k & 1) {
+            sum -= 2 * boarder;
+        }
+        return sum;
+    }
+};
