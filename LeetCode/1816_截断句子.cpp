d452fad299b532c9e5b9b0936d281f1b2ce57960
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-12-06 14:02:08 +0800

    add 1816

---
+++ LeetCode/1816_截断句子.cpp
@@ -0,0 +1,17 @@
+// 1816_½Ø¶Ï¾ä×Ó.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    string truncateSentence(const string& s, int k) {
+        for (size_t i = 0; i < s.size(); i++) {
+            if (s[i] == ' ') {
+                if (--k == 0) {
+                    return s.substr(0, i);
+                }
+            }
+        }
+        return s;
+    }
+};
