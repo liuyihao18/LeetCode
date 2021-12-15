bf7978252c7e63ac98eb45540af243ab75c89213
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-12-15 10:17:34 +0800

    add 851

---
+++ LeetCode/851_喧闹和富有.cpp
@@ -0,0 +1,35 @@
+// 851_ÐúÄÖºÍ¸»ÓÐ.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    int _dfs(int i, const vector<vector<int>>& g, const vector<int>& w, vector<bool>& v, vector<int>& r) {
+        if (v[i]) {
+            return r[i];
+        }
+        v[i] = true;
+        int min_i = i;
+        for (const auto& e : g) {
+            if (e.back() == i) {
+                int j = _dfs(e.front(), g, w, v, r);
+                if (w[j] < w[min_i]) {
+                    min_i = j;
+                }
+            }
+        }
+        r[i] = min_i;
+        return r[i];
+    }
+public:
+    vector<int> loudAndRich(const vector<vector<int>>& richer, const vector<int>& quiet) {
+        vector<bool> v(quiet.size());
+        vector<int> r(quiet.size());
+        for (size_t i = 0; i < r.size(); i++) {
+            r[i] = static_cast<int>(i);
+        }
+        for (size_t i = 0; i < r.size(); i++) {
+            _dfs(static_cast<int>(i), richer, quiet, v, r);
+        }
+        return r;
+    }
+};
