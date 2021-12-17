3d2029a307e8320161d49e8567ad53bf8614816a
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-12-17 15:57:02 +0800

    add 1518

---
+++ LeetCode/1518_换酒问题.cpp
@@ -0,0 +1,16 @@
+// 1518_»»¾ÆÎÊÌâ.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int res = numBottles;
+        int empty = numBottles;
+        while (empty >= numExchange) {
+            res += empty / numExchange;
+            empty = empty % numExchange + empty / numExchange;
+        }
+        return res;
+    }
+};
