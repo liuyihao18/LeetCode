43c7f7c55b262f474ea37de44dc5adb4d9b45d31
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-12-19 10:00:00 +0800

    add 997

---
+++ LeetCode/997_找到小镇的法官.cpp
@@ -0,0 +1,23 @@
+// 997_找到小镇的法官.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int findJudge(int n, const vector<vector<int>>& trust) {
+        vector<int> inDegree(n + 1, 0);
+        vector<int> outDegree(n + 1, 0);
+        for (const auto& edge : trust) {
+            ++outDegree[edge.front()];
+        }
+        for (const auto& edge : trust) {
+            ++inDegree[edge.back()];
+        }
+        for (int i = 1; i <= n; i++) {
+            if (inDegree[i] == n - 1 && outDegree[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
