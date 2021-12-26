7f761bf774d6346f214096174e9be8dd06c94066
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-12-26 10:48:03 +0800

    add 1078

---
+++ LeetCode/1078_Bigram_分词.cpp
@@ -0,0 +1,48 @@
+// 1078_Bigram_·Ö´Ê.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<string> findOcurrences(string text, string first, string second) {
+        istringstream is(text);
+        string temp;
+        vector<string> result;
+        int state = 0;
+        while (is >> temp) {
+            switch (state) {
+            case 0:
+                if (temp == first) {
+                    state = 1;
+                }
+                break;
+            case 1:
+                if (temp == second) {
+                    state = 2;
+                }
+                else if (temp == first) {
+                    state = 1;
+                }
+                else {
+                    state = 0;
+                }
+                break;
+            case 2:
+                result.push_back(temp);
+                if (second == first && temp == second) {
+                    state = 2;
+                }
+                else if (temp == first) {
+                    state = 1;
+                }
+                else {
+                    state = 0;
+                }
+                break;
+            default:
+                break;
+            }
+        }
+        return result;
+    }
+};
