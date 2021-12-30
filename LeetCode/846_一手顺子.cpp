ce2eeb882dbf5f77c7a7668d0f7b4c505724466f
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-12-30 09:37:24 +0800

    add 846

---
+++ LeetCode/846_一手顺子.cpp
@@ -0,0 +1,38 @@
+// 846_Ò»ÊÖË³×Ó.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool isNStraightHand(const vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) {
+            return false;
+        }
+        map<int, size_t> handCount;
+        for (auto a : hand) {
+            handCount[a]++;
+        }
+        for (auto iter = handCount.begin(); iter != handCount.end();) {
+            if (!iter->second) {
+                iter = handCount.erase(iter);
+                continue;
+            }
+            int a = iter->first;
+            map<int, size_t>::iterator pos = iter;
+            for (int i = 0; i < groupSize; i++, pos++) {
+                if (pos == handCount.end()) {
+                    return false;
+                }
+                else {
+                    if (pos->first != a + i || !pos->second) {
+                        return false;
+                    }
+                    else {
+                        pos->second--;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
