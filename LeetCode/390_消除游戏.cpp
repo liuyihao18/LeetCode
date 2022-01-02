f74888cd58e1c17c316f816ce62d88a912e1a1fe
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-01-02 10:04:32 +0800

    add 390

---
+++ LeetCode/390_消除游戏.cpp
@@ -0,0 +1,26 @@
+// 390_Ïû³ýÓÎÏ·.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int lastRemaining(int n) {
+        int d = 1;
+        int a1 = 1;
+        int direction = 1;
+        while (n > 1) {
+            if (direction & 1) {
+                a1 += d;
+            }
+            else {
+                if (n & 1) {
+                    a1 += d;
+                }
+            }
+            n /= 2;
+            d *= 2;
+            direction ^= 1;
+        }
+        return a1;
+    }
+};
