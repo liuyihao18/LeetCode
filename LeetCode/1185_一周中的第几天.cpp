9556f77ea5a91a4930a2e0917146a294828d56ea
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-01-03 10:17:42 +0800

    add 1185

---
+++ LeetCode/1185_一周中的第几天.cpp
@@ -0,0 +1,16 @@
+// 1185_一周中的第几天.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    const string num2str[7] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+public:
+    string dayOfTheWeek(int day, int month, int year) {
+        tm t{};
+        t.tm_year = year - 1900;
+        t.tm_mon = month - 1;
+        t.tm_mday = day;
+        mktime(&t);
+        return num2str[t.tm_wday];
+    }
+};
