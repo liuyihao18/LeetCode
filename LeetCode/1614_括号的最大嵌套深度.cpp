f0e0b4241918667781bac0944b6517ddcf4f0f05
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-01-07 10:10:15 +0800

    add 1614

---
+++ LeetCode/1614_括号的最大嵌套深度.cpp
@@ -0,0 +1,15 @@
+// 1614_括号的最大嵌套深度.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maxDepth(string s) {
+        int max_depth = 0;
+        int depth = 0;
+        for (auto c : s)
+            if (c == '(') max_depth = max(max_depth, ++depth);
+            else if (c == ')') --depth;
+        return max_depth;
+    }
+};
