076277ee7ab9f1210b67aa1b49b83230856bb337
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-01-09 10:06:37 +0800

    add 1629

---
+++ LeetCode/1629_按键持续时间最长的键.cpp
@@ -0,0 +1,23 @@
+// 1629_按键持续时间最长的键.cpp
+#include "custom.h"
+ustd
+
+constexpr size_t INDEX(char c) {
+    return static_cast<size_t>(c) - 'a';
+}
+
+class Solution {
+public:
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+        vector<int> keyTime(26, 0);
+        size_t i = 0;
+        keyTime[INDEX(keysPressed[i])] = releaseTimes[i];
+        i++;
+        for (; i < keysPressed.size(); i++) {
+            keyTime[INDEX(keysPressed[i])] = 
+                max(keyTime[INDEX(keysPressed[i])], releaseTimes[i] - releaseTimes[i - 1]);
+        }
+        auto iter = max_element(keyTime.rbegin(), keyTime.rend());
+        return static_cast<char>(distance(iter, --keyTime.rend())) + 'a';
+    }
+};
