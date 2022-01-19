a10978e680ec5c7dc03f019708eaf94743f36f30
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-01-19 08:42:02 +0800

    add 219

---
+++ LeetCode/219_存在重复元素_II.cpp
@@ -0,0 +1,20 @@
+// 219_´æÔÚÖØ¸´ÔªËØ_II.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        map<int, size_t> numsIndex;
+        for (size_t i = 0; i < nums.size(); i++) {
+            auto iter = numsIndex.find(nums[i]);
+            if (iter != numsIndex.end()) {
+                if (i - iter->second <= k) {
+                    return true;
+                }
+            }
+            numsIndex[nums[i]] = i;
+        }
+        return false;
+    }
+};
