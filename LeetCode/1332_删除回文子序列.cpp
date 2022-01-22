d33cb26f96e0dfa3c4d91164b9b2234d470f880f
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-01-22 09:31:21 +0800

    add 1332

---
+++ LeetCode/1332_删除回文子序列.cpp
@@ -0,0 +1,30 @@
+// 1332_删除回文子序列.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    bool isPalindrome(const string& s) {
+        auto iter1 = s.begin();
+        auto iter2 = s.end();
+        --iter2;
+        while (iter1 <= iter2) {
+            if (*iter1 != *iter2) {
+                return false;
+            }
+            else {
+                ++iter1;
+                --iter2;
+            }
+        }
+        return true;
+    }
+public:
+    int removePalindromeSub(string s) {
+        if (isPalindrome(s)) {
+            return 1;
+        }
+        else {
+            return 2;
+        }
+    }
+};
