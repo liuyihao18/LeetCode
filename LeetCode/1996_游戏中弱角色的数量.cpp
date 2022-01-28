e73e727fcaa0d9c60f74689045ce3d95e852bd92
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-01-28 10:51:07 +0800

    add 1996

--- LeetCode/1996_游戏中弱角色的数量.cpp
+++ LeetCode/1996_游戏中弱角色的数量.cpp
@@ -5,6 +5,8 @@ ustd
 class Solution {
 public:
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
+        // 攻击值降序排列，防御值升序排列
+        // 保证若防御值比之前防御值小，则攻击值一定比之前小
         sort(properties.begin(), properties.end(), [](const vector<int>& v1, const vector<int>& v2) {
             return (v1[0] == v2[0]) ? (v1[1] < v2[1]) : (v1[0] > v2[0]);
             });
