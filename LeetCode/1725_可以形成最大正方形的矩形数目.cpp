42cda66f415b630631936a904e212920b5c37d96
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-02-04 09:51:03 +0800

    add 1725

---
+++ LeetCode/1725_可以形成最大正方形的矩形数目.cpp
@@ -0,0 +1,20 @@
+// 1725_可以形成最大正方形的矩形数目.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int countGoodRectangles(vector<vector<int>>& rectangles) {
+        pair<int, int> max_square = { 0,0 };
+        for (const auto& rectangle : rectangles) {
+            int square_len = min(rectangle.front(), rectangle.back());
+            if (square_len > max_square.first) {
+                max_square = { square_len, 1 };
+            }
+            else if (square_len == max_square.first) {
+                max_square.second++;
+            }
+        }
+        return max_square.second;
+    }
+};
