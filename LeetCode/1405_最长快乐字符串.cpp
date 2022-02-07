f8cc69427be512e1be325248c3a485dd6706f2f2
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-02-07 10:21:06 +0800

    add 1405

---
+++ LeetCode/1405_最长快乐字符串.cpp
@@ -0,0 +1,74 @@
+// 1405_×î³¤¿ìÀÖ×Ö·û´®.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        string result;
+        char ll = 0;
+        char l = 0;
+        while (a > 0 || b > 0 || c > 0) {
+            int* max = nullptr;
+            char curr = 0;
+            if (a >= b && a > c) {
+                max = &a;
+                curr = 'a';
+            }
+            else if (b >= a && b > c) {
+                max = &b;
+                curr = 'b';
+            }
+            else {
+                max = &c;
+                curr = 'c';
+            }
+            if (curr == ll && curr == l) {
+                if (max == &a) {
+                    if (b >= c && b > 0) {
+                        max = &b;
+                        curr = 'b';
+                    }
+                    else if(c >= b && c > 0) {
+                        max = &c;
+                        curr = 'c';
+                    }
+                    else {
+                        break;
+                    }
+                }
+                else if (max == &b) {
+                    if (a >= c && a > 0) {
+                        max = &a;
+                        curr = 'a';
+                    }
+                    else if (c >= a && c > 0) {
+                        max = &c;
+                        curr = 'c';
+                    }
+                    else {
+                        break;
+                    }
+                }
+                else {
+                    if (a >= b && a > 0) {
+                        max = &a;
+                        curr = 'a';
+                    }
+                    else if (b >= a && b > 0) {
+                        max = &b;
+                        curr = 'b';
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+            (*max)--;
+            result += curr;
+            ll = l;
+            l = curr;
+        }
+        return result;
+    }
+};
