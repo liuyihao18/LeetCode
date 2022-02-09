5558096bac9dd3846e5714f8fb8a22b19ea040b0
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-02-09 09:56:14 +0800

    add 2006

---
+++ LeetCode/2006_差的绝对值为K的数对数目.cpp
@@ -0,0 +1,20 @@
+// 2006_差的绝对值为K的数对数目.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        map<int, int> m;
+        for (auto num : nums) {
+            m[num]++;
+        }
+        int result = 0;
+        for (const auto& [key, val] : m) {
+            if (m.count(key + k)) {
+                result += val * m[key + k];
+            }
+        }
+        return result;
+    }
+};
