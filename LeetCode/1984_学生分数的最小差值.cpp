187b6ab8c1c136b89fa60ab5163142ad349e697a
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-02-11 09:57:28 +0800

    add 1984

---
+++ LeetCode/1984_学生分数的最小差值.cpp
@@ -0,0 +1,22 @@
+// 1984_学生分数的最小差值.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        size_t i = 0;
+        size_t j = k;
+        j--;
+        int min_diff = INT_MAX;
+        while (j < nums.size()) {
+            int min_num = nums[i];
+            int max_num = nums[j];
+            min_diff = min(min_diff, max_num - min_num);
+            i++;
+            j++;
+        }
+        return min_diff;
+    }
+};
