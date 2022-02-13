fc327161673d7a87ac7fd5397891430665169e3d
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-02-13 10:54:31 +0800

    add 1189

---
+++ LeetCode/1189_“气球”的最大数量.cpp
@@ -0,0 +1,26 @@
+// 1189_“气球”的最大数量.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        vector<int> counts(128, 0);
+        for (auto c : text) {
+            counts[c]++;
+        }
+        map<char, int> needs{
+            // ballon
+            {'a', 1},
+            {'b', 1},
+            {'l', 2},
+            {'n', 1},
+            {'o', 2},
+        };
+        int result = INT_MAX;
+        for (const auto& [key, val] : needs) {
+            result = min(result, counts[key] / val);
+        }
+        return result;
+    }
+};
