de9cbdf41be883353b2d06153b780cd8100fb1f2
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-02-15 10:24:41 +0800

    add 1380

---
+++ LeetCode/1380_矩阵中的幸运数.cpp
@@ -0,0 +1,27 @@
+// 1380_矩阵中的幸运数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> luckyNumbers(vector<vector<int>>& matrix) {
+        size_t m = matrix.size();
+        size_t n = matrix.front().size();
+        vector<int> row_min(m, INT_MAX);
+        vector<int> col_max(n, INT_MIN);
+        for (size_t i = 0; i < m; i++) {
+            for (size_t j = 0; j < n; j++) {
+                row_min[i] = min(row_min[i], matrix[i][j]);
+                col_max[j] = max(col_max[j], matrix[i][j]);
+            }
+        }
+        vector<int> result;
+        sort(col_max.begin(), col_max.end());
+        for (auto num : row_min) {
+            if (*lower_bound(col_max.begin(), col_max.end(), num) == num) {
+                result.push_back(num);
+            }
+        }
+        return result;
+    }
+};
