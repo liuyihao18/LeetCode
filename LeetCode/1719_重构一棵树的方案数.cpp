f86ef9d523e258ad7e1f58b5dff48f1e27bef41a
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-02-16 09:24:58 +0800

    add 1719

---
+++ LeetCode/1719_重构一棵树的方案数.cpp
@@ -0,0 +1,69 @@
+// 1719_重构一棵树的方案数.cpp
+#include "custom.h"
+ustd
+
+/*
+作者：LeetCode - Solution
+链接：https ://leetcode-cn.com/problems/number-of-ways-to-reconstruct-a-tree/solution/zhong-gou-yi-ke-shu-de-fang-an-shu-by-le-36e1/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+// TODO: Exspecially Hard.
+
+class Solution {
+public:
+    int checkWays(vector<vector<int>>& pairs) {
+        unordered_map<int, unordered_set<int>> adj;
+        for (auto& p : pairs) {
+            adj[p[0]].emplace(p[1]);
+            adj[p[1]].emplace(p[0]);
+        }
+        /* 检测是否存在根节点*/
+        int root = -1;
+        for (auto& [node, neighbours] : adj) {
+            if (neighbours.size() == adj.size() - 1) {
+                root = node;
+                break;
+            }
+        }
+        if (root == -1) {
+            return 0;
+        }
+
+        int res = 1;
+        for (auto& [node, neighbours] : adj) {
+            if (node == root) {
+                continue;
+            }
+            int currDegree = static_cast<int>(neighbours.size());
+            int parent = -1;
+            int parentDegree = INT_MAX;
+
+            /* 根据 degree 的大小找到 node 的父节点 parent */
+            for (auto& neighbour : neighbours) {
+                if (adj[neighbour].size() < parentDegree && adj[neighbour].size() >= currDegree) {
+                    parent = neighbour;
+                    parentDegree = static_cast<int>(adj[neighbour].size());
+                }
+            }
+            if (parent == -1) {
+                return 0;
+            }
+
+            /* 检测 neighbours 是否是 adj[parent] 的子集 */
+            for (auto& neighbour : neighbours) {
+                if (neighbour == parent) {
+                    continue;
+                }
+                if (!adj[parent].count(neighbour)) {
+                    return 0;
+                }
+            }
+            if (parentDegree == currDegree) {
+                res = 2;
+            }
+        }
+        return res;
+    }
+};
