d53047796ed8a386b635549f26bef21ec8a7b65b
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-02-17 10:05:02 +0800

    add 688

---
+++ LeetCode/688_骑士在棋盘上的概率.cpp
@@ -0,0 +1,37 @@
+// 688_骑士在棋盘上的概率.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<vector<int>> dirs = { {-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2} };
+
+    double knightProbability(int n, int k, int row, int column) {
+        vector<vector<vector<double>>> dp(k + 1, vector<vector<double>>(n, vector<double>(n)));
+        for (int step = 0; step <= k; step++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (step == 0) {
+                        dp[step][i][j] = 1;
+                    }
+                    else {
+                        for (auto& dir : dirs) {
+                            int ni = i + dir[0], nj = j + dir[1];
+                            if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                                dp[step][i][j] += dp[step - 1][ni][nj] / 8;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return dp[k][row][column];
+    }
+};
+
+/*
+作者：LeetCode - Solution
+链接：https ://leetcode-cn.com/problems/knight-probability-in-chessboard/solution/qi-shi-zai-qi-pan-shang-de-gai-lu-by-lee-2qhk/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
