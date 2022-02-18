8cc7e68b53b9ba8c13ccd746db18a8ab945d3f6d
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-02-18 16:48:26 +0800

    add 1791

---
+++ LeetCode/1791_找出星型图的中心节点.cpp
@@ -0,0 +1,15 @@
+// 1791_找出星型图的中心节点.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        const auto& edge1 = edges[0];
+        const auto& edge2 = edges[1];
+        int result = edge1[0];
+        return find_if(edge2.begin(), edge2.end(), [=](int i) {
+            return i == result;
+            }) != edge2.end() ? result : edge1[1];
+    }
+};
