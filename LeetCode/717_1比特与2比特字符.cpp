3d60996f268670d2eabfd28577a9ca3e02070c2e
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-02-20 09:36:17 +0800

    add 717

---
+++ LeetCode/717_1比特与2比特字符.cpp
@@ -0,0 +1,30 @@
+// 717_1比特与2比特字符.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int state = 0;
+        bool last = false;
+        for (auto bit : bits) {
+            switch (state) {
+            case 0:
+                if (bit) {
+                    state = 1;
+                }
+                else {
+                    last = true;
+                }
+                break;
+            case 1:
+                state = 0;
+                last = false;
+                break;
+            default:
+                break;
+            }
+        }
+        return last;
+    }
+};
