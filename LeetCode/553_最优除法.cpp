baaee796e367669350dfae11830a46bc4122fa58
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-02-27 10:35:44 +0800

    add 553

---
+++ LeetCode/553_最优除法.cpp
@@ -0,0 +1,51 @@
+// 553_最优除法.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    string doDivision(vector<int>& nums, size_t i, size_t j, double& result, bool maximum) {
+        if (j - i == 1) {
+            result = nums[i] * 1.0;
+            return to_string(nums[i]);
+        }
+        double dividend = 0;
+        double divisor = 0;
+        string _dividend;
+        string _divisor;
+        result = maximum ? 0 : INT_MAX;
+        size_t max_l = 0;
+        for (size_t l = 1; l < j - i; l++) {
+            string temp1 = doDivision(nums, i, i + l, dividend, maximum);
+            string temp2 = doDivision(nums, i + l, j, divisor, !maximum);
+            if (maximum) {
+                if (dividend / divisor > result) {
+                    result = dividend / divisor;
+                    _dividend = temp1;
+                    _divisor = temp2;
+                    max_l = l;
+                }
+            }
+            else {
+                if (dividend / divisor < result) {
+                    result = dividend / divisor;
+                    _dividend = temp1;
+                    _divisor = temp2;
+                    max_l = l;
+                }
+            }
+        }
+        if (max_l + 1 == j - i) {
+            return _dividend + "/" + _divisor;
+        }
+        else {
+            return _dividend + "/(" + _divisor + ")";
+        }
+    }
+public:
+    string optimalDivision(vector<int>& nums) {
+        double _ = 0;
+        return doDivision(nums, 0, nums.size(), _, true);
+        // 虽然我也觉得是 num1 / (num2 / num3 / num4 / ...) 的形式结果最大来着，因为都是整数
+    }
+};
+ 
