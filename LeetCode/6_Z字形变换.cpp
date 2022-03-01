ce8f359bb824035290ecb94cd63f6bfeab64679c
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-03-01 20:08:59 +0800

    add 6

---
+++ LeetCode/6_Z字形变换.cpp
@@ -0,0 +1,26 @@
+// 6_Z×ÖÐÎ±ä»».cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        vector<string> results(numRows);
+        size_t pos = 0;
+        while (pos < s.size()) {
+            for (int i = 0; i < numRows; i++) {
+                if (pos >= s.size()) {
+                    break;
+                }
+                results[i].push_back(s[pos++]);
+            }
+            for (int i = numRows - 2; i > 0; i--) {
+                if (pos >= s.size()) {
+                    break;
+                }
+                results[i].push_back(s[pos++]);
+            }
+        }
+        return accumulate(results.begin(), results.end(), string());
+    }
+};
