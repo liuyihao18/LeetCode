fb0a14d95c8cfb607620648663aa72292a83dc33
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-03-04 09:33:32 +0800

    add 2104

---
+++ LeetCode/2104_子数组范围和.cpp
@@ -0,0 +1,20 @@
+// 2104_子数组范围和.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        long long result = 0;
+        for (size_t i = 0; i < nums.size(); i++) {
+            int minimum = nums[i];
+            int maximum = nums[i];
+            for (size_t j = i + 1; j < nums.size(); j++) {
+                minimum = min(minimum, nums[j]);
+                maximum = max(maximum, nums[j]);
+                result += static_cast<long long>(maximum) - static_cast<long long>(minimum);
+            }
+        }
+        return result;
+    }
+};
