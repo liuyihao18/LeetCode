b4f523656d09d5e061ab555998aa90bbe67b6424
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-03-05 10:44:19 +0800

    add 521

---
+++ LeetCode/521_最长特殊序列_Ⅰ.cpp
@@ -0,0 +1,10 @@
+// 521_最长特殊序列_Ⅰ.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        return a == b ? -1 : static_cast<int>(max(a.size(), b.size()));
+    }
+};
