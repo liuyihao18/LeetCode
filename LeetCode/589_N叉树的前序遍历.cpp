079713a0ea78af7ee638b69e9945b4c5a9158b80
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-03-11 09:43:51 +0800

    add 589

---
+++ LeetCode/589_N叉树的前序遍历.cpp
@@ -0,0 +1,38 @@
+// 589_N²æÊ÷µÄÇ°Ðò±éÀú.cpp
+#include "custom.h"
+ustd
+
+// Definition for a Node.
+class Node {
+public:
+    int val{ 0 };
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+    void preOrder(Node* root, vector<int>& result) {
+        if (root) {
+            result.push_back(root->val);
+            for (auto child : root->children) {
+                preOrder(child, result);
+            }
+        }
+    }
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> result;
+        preOrder(root, result);
+        return result;
+    }
+};
