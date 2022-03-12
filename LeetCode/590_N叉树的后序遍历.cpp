829452696a67fa7e90ab87599c0716888c3d0faa
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-03-12 09:38:00 +0800

    add 590

---
+++ LeetCode/590_N叉树的后序遍历.cpp
@@ -0,0 +1,38 @@
+// 590_N²æÊ÷µÄºóÐò±éÀú.cpp
+#include "custom.h"
+ustd
+
+// Definition for a Node.
+class Node {
+public:
+    int val{ 0 };
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+    void postOrder(Node* root, vector<int>& result) {
+        if (root) {
+            for (auto child : root->children) {
+                postOrder(child, result);
+            }
+            result.push_back(root->val);
+        }
+    }
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> result;
+        postOrder(root, result);
+        return result;
+    }
+};
