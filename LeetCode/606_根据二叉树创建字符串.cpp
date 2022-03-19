e6bfaa5a17aac4c56f431c8718c0d1be3dc5e4eb
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-03-19 09:56:39 +0800

    add 606

---
+++ LeetCode/606_根据二叉树创建字符串.cpp
@@ -0,0 +1,36 @@
+// 606_根据二叉树创建字符串.cpp
+#include "custom.h"
+ustd
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        string result = to_string(root->val);
+        if (root->right) {
+            result += '(';
+            if (root->left) {
+                result += tree2str(root->left);
+            }
+            result += ')';
+            result += '(';
+            result += tree2str(root->right);
+            result += ')';
+        }
+        else if (root->left) {
+            result += '(';
+            result += tree2str(root->left);
+            result += ')';
+        }
+        return result;
+    }
+};
