fdff1f66e2ab65a0d47657d43e1a364c46cf5782
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-03-20 13:43:53 +0800

    add 2039

---
+++ LeetCode/2039_网络空闲的时刻.cpp
@@ -0,0 +1,38 @@
+// 2039_ÍøÂç¿ÕÏÐµÄÊ±¿Ì.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
+        // ±ßÁ´±í
+        size_t n = patience.size();
+        vector<vector<int>> graph(n);
+        for (const auto& edge : edges) {
+            // Ë«Ïò±ß
+            graph[edge.front()].push_back(edge.back());
+            graph[edge.back()].push_back(edge.front());
+        }
+        // ¾àÀë
+        vector<int> distances(n, -1);
+        queue<pair<int, int>> q;
+        q.push({ 0,0 });
+        while (!q.empty()) {
+            pair<int, int> curr = q.front();
+            q.pop();
+            if (distances[curr.first] != -1) {
+                continue;
+            }
+            distances[curr.first] = curr.second;
+            for (auto e : graph[curr.first]) {
+                q.push({ e, curr.second + 1 });
+            }
+        }
+        int result = 0;
+        for (size_t i = 1; i < n; i++) {
+            int time = patience[i] * ((2 * distances[i] - 1) / patience[i]) + 2 * distances[i] + 1;
+            result = max(result, time);
+        }
+        return result;
+    }
+};
