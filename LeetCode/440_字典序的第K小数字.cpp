22fdbd0f07a56f01ee6db1eccaada6bb10b731c2
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-03-23 14:16:29 +0800

    add 440

---
+++ LeetCode/440_字典序的第K小数字.cpp
@@ -0,0 +1,41 @@
+// 440_字典序的第K小数字.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int getSteps(int curr, long n) {
+        int steps = 0;
+        long first = curr;
+        long last = curr;
+        while (first <= n) {
+            steps += min(last, n) - first + 1;
+            first = first * 10;
+            last = last * 10 + 9;
+        }
+        return steps;
+    }
+
+    int findKthNumber(int n, int k) {
+        int curr = 1;
+        k--;
+        while (k > 0) {
+            int steps = getSteps(curr, n);
+            if (steps <= k) {
+                k -= steps;
+                curr++;
+            }
+            else {
+                curr = curr * 10;
+                k--;
+            }
+        }
+        return curr;
+    }
+};
+/*
+作者：LeetCode - Solution
+链接：https ://leetcode-cn.com/problems/k-th-smallest-in-lexicographical-order/solution/zi-dian-xu-de-di-kxiao-shu-zi-by-leetcod-bfy0/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
