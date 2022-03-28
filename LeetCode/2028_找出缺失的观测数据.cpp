1089f565b7c1e1c52e9a4a7f00435f2226f7c835
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-03-28 16:35:47 +0800

    add 2028

---
+++ LeetCode/2028_找出缺失的观测数据.cpp
@@ -0,0 +1,31 @@
+// 2028_找出缺失的观测数据.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int m = static_cast<int>(rolls.size());
+        int total = mean * (m + n);
+        for (auto roll : rolls) {
+            total -= roll;
+        }
+        if (total < n || total > 6 * n) {
+            return {};
+        }
+        vector<int> result;
+        int i = 0;
+        while (i < n && total > (n - i)) {
+            int temp = min(6, total - (n - i - 1));
+            result.push_back(temp);
+            total -= temp;
+            i++;
+        }
+        while (i < n) {
+            result.push_back(1);
+            total--;
+            i++;
+        }
+        return result;
+    }
+};
