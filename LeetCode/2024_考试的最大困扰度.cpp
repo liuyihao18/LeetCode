3d927eda86f31f31a1ca1ceecd2bf03a308c45e5
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-03-29 19:51:49 +0800

    add 2024

---
+++ LeetCode/2024_考试的最大困扰度.cpp
@@ -0,0 +1,24 @@
+// 2024_考试的最大困扰度.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    int maxConsecutiveChar(string& answerKey, int k, char ch) {
+        size_t n = answerKey.size();
+        size_t ans = 0;
+        for (size_t left = 0, right = 0, sum = 0; right < n; right++) {
+            sum += answerKey[right] != ch;
+            while (sum > k) {
+                sum -= answerKey[left] != ch;
+                left++;
+            }
+            ans = max(ans, right - left + 1);
+        }
+        return static_cast<int>(ans);
+    }
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        return max(maxConsecutiveChar(answerKey, k, 'T'),
+            maxConsecutiveChar(answerKey, k, 'F'));
+    }
+};
