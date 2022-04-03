338f57dbbdcb0fdcc4e51c04ad38a00f403928ad
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-04-03 10:20:07 +0800

    add 744

---
+++ LeetCode/744_寻找比目标字母大的最小字母.cpp
@@ -0,0 +1,13 @@
+// 744_寻找比目标字母大的最小字母.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        if (target >= letters.back()) {
+            return letters.front();
+        }
+        return *upper_bound(letters.begin(), letters.end(), target);
+    }
+};
