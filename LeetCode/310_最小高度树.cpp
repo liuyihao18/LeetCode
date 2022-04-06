1eb7b3f531db830f80255e2d7ca985b034c55c33
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-04-06 13:53:13 +0800

    add 310

---
+++ LeetCode/310_最小高度树.cpp
@@ -0,0 +1,52 @@
+// 310_最小高度树.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if (n == 1) {
+            return { 0 };
+        }
+        vector<int> degree(n);
+        vector<vector<int>> adj(n);
+        for (auto& edge : edges) {
+            adj[edge[0]].emplace_back(edge[1]);
+            adj[edge[1]].emplace_back(edge[0]);
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+        }
+        queue<int> qu;
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (degree[i] == 1) {
+                qu.emplace(i);
+            }
+        }
+        int remainNodes = n;
+        while (remainNodes > 2) {
+            int sz = static_cast<int>(qu.size());
+            remainNodes -= sz;
+            for (int i = 0; i < sz; i++) {
+                int curr = qu.front();
+                qu.pop();
+                for (auto& v : adj[curr]) {
+                    if (--degree[v] == 1) {
+                        qu.emplace(v);
+                    }
+                }
+            }
+        }
+        while (!qu.empty()) {
+            ans.emplace_back(qu.front());
+            qu.pop();
+        }
+        return ans;
+    }
+};
+/*
+作者：LeetCode - Solution
+链接：https ://leetcode-cn.com/problems/minimum-height-trees/solution/zui-xiao-gao-du-shu-by-leetcode-solution-6v6f/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
