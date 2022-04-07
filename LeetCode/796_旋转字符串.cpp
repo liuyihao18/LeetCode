15ff5e53653fa8e4c22371c96122e50ea22dc97a
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-04-07 12:57:17 +0800

    add 796

---
+++ LeetCode/796_旋转字符串.cpp
@@ -0,0 +1,25 @@
+// 796_Ðý×ª×Ö·û´®.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.size() != goal.size()) {
+            return 0;
+        }
+        size_t n = s.size();
+        for (size_t i = 0; i < n; i++) {
+            size_t j = 0;
+            for (; j < n; j++) {
+                if (goal[j] != s[(i + j) % n]) {
+                    break;
+                }
+            }
+            if (j == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
