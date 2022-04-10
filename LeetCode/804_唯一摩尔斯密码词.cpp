a104378e88a50ca301fc0480be2d4797ec3f48df
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-04-10 11:00:21 +0800

    add 804

---
+++ LeetCode/804_唯一摩尔斯密码词.cpp
@@ -0,0 +1,20 @@
+// 804_Î¨Ò»Ä¦¶ûË¹ÃÜÂë´Ê.cpp
+#include "custom.h"
+ustd
+
+const string char2str[] = { ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.." };
+
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        set<string> s;
+        for (const auto& word : words) {
+            string code;
+            for (auto c : word) {
+                code += char2str[c - 'a'];
+            }
+            s.insert(code);
+        }
+        return static_cast<int>(s.size());
+    }
+};
