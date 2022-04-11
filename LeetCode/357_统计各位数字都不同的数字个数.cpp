5e595e83f9172aa5f383919aa7666b13028559b7
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-04-11 16:22:59 +0800

    add 357

---
+++ LeetCode/357_统计各位数字都不同的数字个数.cpp
@@ -0,0 +1,19 @@
+// 357_统计各位数字都不同的数字个数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        int result = 0;
+        for (; n > 0; n--) {
+            int temp = 9;
+            for (int i = 1; i < n; i++) {
+                temp *= (10 - i);
+            }
+            result += temp;
+        }
+        result++; // n = 0
+        return result;
+    }
+};
