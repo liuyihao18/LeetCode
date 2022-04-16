385424573b5dbbd9b8c05e58ccadf380585a8fa1
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-04-16 10:30:17 +0800

    add 479

---
+++ LeetCode/479_最大回文数乘积.cpp
@@ -0,0 +1,26 @@
+// 479_最大回文数乘积.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int largestPalindrome(int n) {
+        if (n == 1) {
+            return 9;
+        }
+        int upper = static_cast<int>(pow(10, n)) - 1;
+        for (int left = upper;; --left) {
+            long long p = left;
+            for (int x = left; x > 0; x /= 10) {
+                p = p * 10 + x % 10;
+            }
+            for (long long x = upper; x * x >= p; x--) {
+                if (p % x == 0) {
+                    return p % 1337;
+                }
+            }
+        }
+        int result[] = {9,987,123,597,677,1218,877,475};
+        return result[n - 1];
+    }
+};
