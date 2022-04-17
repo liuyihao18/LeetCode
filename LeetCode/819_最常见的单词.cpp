2a4af9635805af44b7fbf184e8141b514590ecc1
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-04-17 11:19:55 +0800

    add 819

---
+++ LeetCode/819_最常见的单词.cpp
@@ -0,0 +1,29 @@
+// 819_最常见的单词.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        map<string, size_t> counts;
+        string word;
+        paragraph += ' ';
+        for (auto c : paragraph) {
+            if (isalpha(c)) {
+                word += tolower(c);
+            }
+            else {
+                if (!word.empty()) {
+                    counts[word]++;
+                }
+                word.clear();
+            }
+        }
+        for (const auto& word : banned) {
+            counts.erase(word);
+        }
+        return max_element(counts.begin(), counts.end(), [](const pair<string, size_t>& p1, const pair<string, size_t>& p2) {
+            return p1.second < p2.second;
+            })->first;
+    }
+};
