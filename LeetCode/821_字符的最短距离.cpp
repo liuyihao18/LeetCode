91c7cc51951dcfbe1db77c9cfa1ee82a0708ff3f
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-04-19 17:05:56 +0800

    add 821

---
+++ LeetCode/821_字符的最短距离.cpp
@@ -0,0 +1,26 @@
+// 821_×Ö·ûµÄ×î¶Ì¾àÀë.cpp
+#include "custom.h"
+ustd
+
+#define ABS(x, y) static_cast<int>(x < y ? y - x : x - y)
+
+class Solution {
+public:
+    vector<int> shortestToChar(string s, char c) {
+        vector<size_t> pos;
+        for (size_t i = 0; i < s.size(); i++) {
+            if (s[i] == c) {
+                pos.push_back(i);
+            }
+        }
+        vector<int> answer;
+        size_t curr_pos_i = 0;
+        for (size_t i = 0; i < s.size(); i++) {
+            if (curr_pos_i + 1 < pos.size() && ABS(i, pos[curr_pos_i]) > ABS(i, pos[curr_pos_i + 1])) {
+                curr_pos_i++;
+            }
+            answer.push_back(ABS(i, pos[curr_pos_i]));
+        }
+        return answer;
+    }
+};
