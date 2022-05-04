31f70f61d459155c32ff3fe489a694216949f21d
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-05-04 10:39:43 +0800

    add 1823

---
+++ LeetCode/1823_找出游戏的获胜者.cpp
@@ -0,0 +1,27 @@
+// 1823_找出游戏的获胜者.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        set<int> s;
+        for (int i = 1; i <= n; i++) {
+            s.insert(i);
+        }
+        auto iter = s.begin();
+        while (s.size() > 1) {
+            for (int i = 1; i < k; i++) {
+                iter++;
+                if (iter == s.end()) {
+                    iter = s.begin();
+                }
+            }
+            iter = s.erase(iter);
+            if (iter == s.end()) {
+                iter = s.begin();
+            }
+        }
+        return *s.begin();
+    }
+};
