b0b981c9cbf8a89883a98028390ed93fb357f7b3
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-05-05 10:24:51 +0800

    add 713

---
+++ LeetCode/713_乘积小于K的子数组.cpp
@@ -0,0 +1,21 @@
+// 713_乘积小于K的子数组.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int result = 0;
+        int prod = 1;
+        // 滑动窗口
+        for (size_t i = 0, j = 0; j < nums.size(); j++) {
+            prod *= nums[j];
+            while (i <= j && prod >= k) {
+                prod /= nums[i];
+                i++;
+            }
+            result += j - i + 1;
+        }
+        return result;
+    }
+};
