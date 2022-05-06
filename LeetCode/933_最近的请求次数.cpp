93cc44b1d0fc7741664c3dae311bf7ef187167bd
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-05-06 09:55:23 +0800

    add 933

---
+++ LeetCode/933_最近的请求次数.cpp
@@ -0,0 +1,25 @@
+// 933_最近的请求次数.cpp
+#include "custom.h"
+ustd
+
+class RecentCounter {
+    queue<int> request;
+public:
+    RecentCounter() {
+
+    }
+
+    int ping(int t) {
+        request.push(t);
+        while (request.front() < t - 3000) {
+            request.pop();
+        }
+        return static_cast<int>(request.size());
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
