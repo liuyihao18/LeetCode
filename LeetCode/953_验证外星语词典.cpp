8902ea9bb350ff413fabdb2aae1c314bc389a6e4
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-05-17 20:35:22 +0800

    add 953

---
+++ LeetCode/953_验证外星语词典.cpp
@@ -0,0 +1,41 @@
+// 953_—È÷§Õ‚–«”Ô¥ µ‰.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        vector<int> dict(128, -1);
+        for (size_t i = 0; i < order.size(); i++) {
+            dict[order[i]] = static_cast<int>(i);
+        }
+        auto cmp = [&](const string& s1, const string& s2) {
+            size_t n = min(s1.size(), s2.size());
+            for (size_t i = 0; i < n; i++) {
+                if (dict[s1[i]] < dict[s2[i]]) {
+                    return -1;
+                }
+                else if(dict[s1[i]] > dict[s2[i]]) {
+                    return 1;
+                }
+            }
+            if (s1.size() < s2.size()) {
+                return -1;
+            }
+            else if (s1.size() > s2.size()) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        };
+        for (size_t i = 0; i + 1 < words.size(); i++) {
+            string word1 = words[i];
+            string word2 = words[i + 1];
+            if (cmp(word1, word2) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
