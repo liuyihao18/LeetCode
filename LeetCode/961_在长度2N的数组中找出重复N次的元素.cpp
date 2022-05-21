eaf64a7c4b8b2e5c3b98a4a2232f09c35bfa216a
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-05-21 19:47:22 +0800

    add 961

---
+++ LeetCode/961_在长度2N的数组中找出重复N次的元素.cpp
@@ -0,0 +1,17 @@
+// 961_在长度2N的数组中找出重复N次的元素.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        set<int> s;
+        for (auto num : nums) {
+            if (s.count(num)) {
+                return num;
+            }
+            s.insert(num);
+        }
+        return -1;
+    }
+};
