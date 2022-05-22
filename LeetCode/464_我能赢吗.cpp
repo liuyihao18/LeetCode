2e5bebcf887c28a62686eddbdc342d1077625096
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-05-22 10:05:08 +0800

    add 464

---
+++ LeetCode/464_我能赢吗.cpp
@@ -0,0 +1,41 @@
+// 464_我能赢吗.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    unordered_map<int, bool> memo;
+
+    bool canIWin(int maxChoosableInteger, int desiredTotal) {
+        if ((1 + maxChoosableInteger) * (maxChoosableInteger) / 2 < desiredTotal) {
+            return false;
+        }
+        return dfs(maxChoosableInteger, 0, desiredTotal, 0);
+    }
+
+    bool dfs(int maxChoosableInteger, int usedNumbers, int desiredTotal, int currentTotal) {
+        if (!memo.count(usedNumbers)) {
+            bool res = false;
+            for (int i = 0; i < maxChoosableInteger; i++) {
+                if (((usedNumbers >> i) & 1) == 0) {
+                    if (i + 1 + currentTotal >= desiredTotal) {
+                        res = true;
+                        break;
+                    }
+                    if (!dfs(maxChoosableInteger, usedNumbers | (1 << i), desiredTotal, currentTotal + i + 1)) {
+                        res = true;
+                        break;
+                    }
+                }
+            }
+            memo[usedNumbers] = res;
+        }
+        return memo[usedNumbers];
+    }
+};
+/*
+作者：LeetCode - Solution
+链接：https ://leetcode.cn/problems/can-i-win/solution/wo-neng-ying-ma-by-leetcode-solution-ef5v/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
