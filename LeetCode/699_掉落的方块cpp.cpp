25d96c7d3c99b21a4916a04c4af029b5b3a01b58
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-05-26 13:23:32 +0800

    add 699

---
+++ LeetCode/699_掉落的方块cpp.cpp
@@ -0,0 +1,34 @@
+// 699_µôÂäµÄ·½¿é.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    struct Square {
+        int left;
+        int right;
+        int height;
+    };
+public:
+    vector<int> fallingSquares(vector<vector<int>>& positions) {
+        vector<Square> squares;
+        vector<int> results;
+        int result = 0;
+        for (size_t i = 0; i < positions.size(); i++) {
+            int left = positions[i].front();
+            int right = left + positions[i].back();
+            int height = right - left;
+            int base = 0;
+            for (size_t j = 0; j < squares.size(); j++) {
+                int overlapping = min(squares[j].right, right) - max(squares[j].left, left);
+                if (overlapping > 0) {
+                    base = max(base, squares[j].height);
+                }
+            }
+            height = base + height;
+            squares.push_back({ left,right,height });
+            result = max(result, height);
+            results.push_back(result);
+        }
+        return results;
+    }
+};
