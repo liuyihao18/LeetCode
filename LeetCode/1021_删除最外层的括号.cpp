830dc3e0df2c08a791a4e0d6ceb1193bfc7b362a
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-05-28 10:39:26 +0800

    add 1021

---
+++ LeetCode/1021_删除最外层的括号.cpp
@@ -0,0 +1,26 @@
+// 1021_É¾³ı×îÍâ²ãµÄÀ¨ºÅ.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        stack<char> st;
+        string result;
+        for (auto c : s) {
+            if (c == '(') {
+                st.push(c);
+                if (st.size() > 1) {
+                    result += c;
+                }
+            }
+            else {
+                st.pop();
+                if (st.size() > 0) {
+                    result += c;
+                }
+            }
+        }
+        return result;
+    }
+};
