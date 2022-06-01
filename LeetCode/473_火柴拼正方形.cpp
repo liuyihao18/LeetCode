ddd9f18fc6a2542533db720295e23d51a8591cb5
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-06-01 21:04:21 +0800

    add 473

---
+++ LeetCode/473_火柴拼正方形.cpp
@@ -0,0 +1,37 @@
+// 473_火柴拼正方形.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool dfs(int index, vector<int>& matchsticks, vector<int>& edges, int len) {
+        if (index == matchsticks.size()) {
+            return true;
+        }
+        for (int i = 0; i < edges.size(); i++) {
+            edges[i] += matchsticks[index];
+            if (edges[i] <= len && dfs(index + 1, matchsticks, edges, len)) {
+                return true;
+            }
+            edges[i] -= matchsticks[index];
+        }
+        return false;
+    }
+
+    bool makesquare(vector<int>& matchsticks) {
+        int totalLen = accumulate(matchsticks.begin(), matchsticks.end(), 0);
+        if (totalLen % 4 != 0) {
+            return false;
+        }
+        sort(matchsticks.begin(), matchsticks.end(), greater<int>()); // 减少搜索量
+
+        vector<int> edges(4);
+        return dfs(0, matchsticks, edges, totalLen / 4);
+    }
+};
+/*
+作者：LeetCode - Solution
+链接：https ://leetcode.cn/problems/matchsticks-to-square/solution/huo-chai-pin-zheng-fang-xing-by-leetcode-szdp/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
