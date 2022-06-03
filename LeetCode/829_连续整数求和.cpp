cf2b75cc17d68a052c5d8bf78cf4edc37953817c
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-06-03 12:26:54 +0800

    add 829

---
+++ LeetCode/829_连续整数求和.cpp
@@ -0,0 +1,16 @@
+// 829_连续整数求和.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int consecutiveNumbersSum(int n) {
+        int result = 0;
+        int upper = (-1 + static_cast<int>(sqrt(1LL + 8LL * n))) / 2;
+        for (int i = 1; i <= upper; i++) {
+            int remainder = (i & 1) ? 0 : i / 2;
+            if (n % i == remainder) result++;
+        }
+        return result;
+    }
+};
