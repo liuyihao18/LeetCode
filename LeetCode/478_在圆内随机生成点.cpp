33a5fc1124f0ccaf31a29c98fc7eb1e3535aa852
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-06-05 12:57:38 +0800

    add 478

---
+++ LeetCode/478_在圆内随机生成点.cpp
@@ -0,0 +1,23 @@
+// 478_在圆内随机生成点.cpp
+#include "custom.h"
+#include <random>
+ustd
+
+class Solution {
+private:
+    mt19937 gen{ random_device{}() };
+    uniform_real_distribution<double> dis;
+    const double PI = acos(-1.0);
+    const double radius;
+    const double x_center;
+    const double y_center;
+public:
+    Solution(double radius, double x_center, double y_center)
+        : dis(0, 1), radius(radius), x_center(x_center), y_center(y_center) { }
+
+    vector<double> randPoint() {
+        double phi = dis(gen) * 2 * PI;
+        double rho = sqrt(dis(gen)) * radius;
+        return { x_center + rho * cos(phi), y_center + rho * sin(phi) };
+    }
+};
