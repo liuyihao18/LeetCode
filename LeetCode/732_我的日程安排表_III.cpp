2ec4029c3ea21b9d8449939a6f2c36f5b68f5ea3
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-06-06 15:30:39 +0800

    add 732

---
+++ LeetCode/732_我的日程安排表_III.cpp
@@ -0,0 +1,35 @@
+// 732_我的日程安排表_III.cpp
+#include "custom.h"
+ustd
+
+class MyCalendarThree {
+    map<int, int> m;
+    int maxBookNum;
+public:
+    MyCalendarThree() : m(), maxBookNum(0) { m[0] = 0; }
+
+    int book(int start, int end) {
+        auto iter1 = m.lower_bound(start);
+        auto iter2 = m.lower_bound(end);
+        if (iter1 == m.end()) {
+            m[start] = 1;
+            m[end] = 0;
+            maxBookNum = max(maxBookNum, 1);
+        }
+        else {
+            for (map<int,int>::iterator iter = iter1; iter != iter2; ++iter) {
+                iter->second++;
+                maxBookNum = max(maxBookNum, iter->second);
+            }
+            if (iter1->first != start) {
+                m[start] = (--iter1)->second + 1;
+                maxBookNum = max(maxBookNum, m[start]);
+            }
+            if (iter2 == m.end() || iter2->first != end) {
+                m[end] = (--iter2)->second - 1;
+                maxBookNum = max(maxBookNum, m[end]);
+            }
+        }
+        return maxBookNum;
+    }
+};
