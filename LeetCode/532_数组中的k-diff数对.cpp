bb49c4ae176eefd08357cd78620b48b4ce5fef12
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-06-16 13:44:40 +0800

    add 532

---
+++ LeetCode/532_数组中的k-diff数对.cpp
@@ -0,0 +1,31 @@
+// 532_数组中的k-diff数对.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int count = 0;
+        for (size_t i = 0, j = 1; j < nums.size();) {
+            while (j < nums.size() && (j <= i || nums[j] - nums[i] < k)) {
+                j++;
+            }
+            if (j < nums.size() && nums[j] - nums[i] == k) {
+                count++;
+                while (i + 1 < nums.size() && nums[i + 1] == nums[i]) {
+                    i++;
+                }
+                i++;
+                while (j + 1 < nums.size() && nums[j + 1] == nums[j]) {
+                    j++;
+                }
+                j++;
+            }
+            else {
+                i++;
+            }
+        }
+        return count;
+    }
+};
