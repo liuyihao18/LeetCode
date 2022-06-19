057f1df71f24c6641eff3a49af82ee30f5e6cd2e
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-06-19 10:01:51 +0800

    add 508

---
+++ LeetCode/508_出现次数最多的子树元素和.cpp
@@ -0,0 +1,46 @@
+// 508_出现次数最多的子树元素和.cpp
+#include "custom.h"
+ustd
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+    int dfs(TreeNode* root, map<int, size_t>& m) {
+        int sum = root->val;
+        if (root->left) {
+            sum += dfs(root->left, m);
+        }
+        if (root->right) {
+            sum += dfs(root->right, m);
+        }
+        m[sum]++;
+        return sum;
+    }
+public:
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        map<int, size_t> m;
+        if (root) {
+            dfs(root, m);
+        }
+        size_t maxVal = 0;
+        for (const auto& [key, val] : m) {
+            maxVal = max(maxVal, val);
+        }
+        vector<int> result;
+        for (const auto& [key, val] : m) {
+            if (val == maxVal) {
+                result.push_back(key);
+            }
+        }
+        return result;
+    }
+};
