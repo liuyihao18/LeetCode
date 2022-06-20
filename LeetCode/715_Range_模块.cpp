4994e821c1511f12f2b4d011d70a231709ae6991
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-06-20 10:37:07 +0800

    add 715

---
+++ LeetCode/715_Range_模块.cpp
@@ -0,0 +1,86 @@
+// 715_Range_模块.cpp
+#include "custom.h"
+ustd
+
+// TODO: 区间操作
+class RangeModule {
+public:
+    RangeModule() {}
+
+    void addRange(int left, int right) {
+        auto it = intervals.upper_bound(left);
+        if (it != intervals.begin()) {
+            auto start = prev(it);
+            if (start->second >= right) {
+                return;
+            }
+            if (start->second >= left) {
+                left = start->first;
+                intervals.erase(start);
+            }
+        }
+        while (it != intervals.end() && it->first <= right) {
+            right = max(right, it->second);
+            it = intervals.erase(it);
+        }
+        intervals[left] = right;
+    }
+
+    bool queryRange(int left, int right) {
+        auto it = intervals.upper_bound(left);
+        if (it == intervals.begin()) {
+            return false;
+        }
+        it = prev(it);
+        return right <= it->second;
+    }
+
+    void removeRange(int left, int right) {
+        auto it = intervals.upper_bound(left);
+        if (it != intervals.begin()) {
+            auto start = prev(it);
+            if (start->second >= right) {
+                int ri = start->second;
+                if (start->first == left) {
+                    intervals.erase(start);
+                }
+                else {
+                    start->second = left;
+                }
+                if (right != ri) {
+                    intervals[right] = ri;
+                }
+                return;
+            }
+            else if (start->second > left) {
+                start->second = left;
+            }
+        }
+        while (it != intervals.end() && it->first < right) {
+            if (it->second <= right) {
+                it = intervals.erase(it);
+            }
+            else {
+                intervals[right] = it->second;
+                intervals.erase(it);
+                break;
+            }
+        }
+    }
+
+private:
+    map<int, int> intervals;
+};
+/*
+作者：LeetCode - Solution
+链接：https ://leetcode.cn/problems/range-module/solution/range-mo-kuai-by-leetcode-solution-4utf/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+/**
+ * Your RangeModule object will be instantiated and called as such:
+ * RangeModule* obj = new RangeModule();
+ * obj->addRange(left,right);
+ * bool param_2 = obj->queryRange(left,right);
+ * obj->removeRange(left,right);
+ */
