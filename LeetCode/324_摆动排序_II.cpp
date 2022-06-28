f9b3b45d7c12981ff80cb520d792853db8552ae7
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-06-28 13:44:33 +0800

    add 324

---
+++ LeetCode/324_摆动排序_II.cpp
@@ -0,0 +1,21 @@
+// 324_°Ú¶¯ÅÅÐò_II.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        vector<int> temp(nums);
+        sort(temp.begin(), temp.end());
+        size_t i = (temp.size() - 1) / 2, j = temp.size() - 1;
+        size_t k = 0;
+        while (k < nums.size()) {
+            if (k < nums.size()) {
+                nums[k++] = temp[i--];
+            }
+            if (k < nums.size()) {
+                nums[k++] = temp[j--];
+            }
+        }
+    }
+};
