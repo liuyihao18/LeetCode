324af553418fbe896276c56da535f751d3068fba
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-06-29 09:43:20 +0800

    add 535

---
+++ LeetCode/535_TinyURL_的加密与解密.cpp
@@ -0,0 +1,31 @@
+// 535_TinyURL_的加密与解密.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    map<int, string> m;
+
+public:
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        int i;
+        while (m.count(i = rand())) {
+            if (m[i] == longUrl) {
+                break;
+            }
+        }
+        m[i] = longUrl;
+        return to_string(i);
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        int i = stoi(shortUrl);
+        return m[i];
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
