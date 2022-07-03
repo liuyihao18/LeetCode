fcc806d62e31a9686a838ff3d79bfc1ff8b7d5c9
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-07-03 10:54:43 +0800

    add 556

---
+++ LeetCode/556_下一个更大元素_III.cpp
@@ -0,0 +1,12 @@
+// 556_下一个更大元素_III.cpp
+#include "custom.h"
+ustd
+
+// TODO: 下一个排列
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        auto&& s = to_string(n);
+        return !next_permutation(s.begin(), s.end()) || stol(s) > INT_MAX ? -1 : stoi(s);
+    }
+};
