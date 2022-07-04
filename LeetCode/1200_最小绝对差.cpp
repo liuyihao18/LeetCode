cef8a749506265f090334eee2a7c1baa4cb5c6fc
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-07-04 09:28:41 +0800

    add 1200

---
+++ LeetCode/1200_最小绝对差.cpp
@@ -0,0 +1,21 @@
+// 1200_×îÐ¡¾ø¶Ô²î.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int minDiff = INT_MAX;
+        for (size_t i = 0; i + 1 < arr.size(); i++) {
+            minDiff = min(minDiff, arr[i + 1] - arr[i]);
+        }
+        vector<vector<int>> result;
+        for (size_t i = 0; i + 1 < arr.size(); i++) {
+            if (arr[i + 1] - arr[i] == minDiff) {
+                result.push_back({ arr[i], arr[i + 1] });
+            }
+        }
+        return result;
+    }
+};
