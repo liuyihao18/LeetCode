64cc17909a271bbb473a4262609155925e839026
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-07-05 09:35:06 +0800

    add 729

---
+++ LeetCode/729_我的日程安排表_I.cpp
@@ -0,0 +1,34 @@
+// 729_我的日程安排表_I.cpp
+#include "custom.h"
+ustd
+
+class MyCalendar {
+    map<int, int> m;
+public:
+    MyCalendar() {
+
+    }
+
+    bool book(int start, int end) {
+        auto iter = m.upper_bound(start);
+        if (iter != m.begin()) {
+            auto pre = prev(iter);
+            if (pre->second > start) {
+                return false;
+            }
+        }
+        if (iter != m.end()) {
+            if (end > iter->first) {
+                return false;
+            }
+        }
+        m[start] = end;
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
