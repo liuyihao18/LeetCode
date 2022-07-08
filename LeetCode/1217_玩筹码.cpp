7186be453e1097dc21a3e4a6cefafcdf273f66a5
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-07-08 10:02:42 +0800

    add 1217

---
+++ LeetCode/1217_玩筹码.cpp
@@ -0,0 +1,19 @@
+// 1217_Íæ³ïÂë.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int odd = 0;
+        int even = 0;
+        for (auto pos : position) pos & 1 ? odd++ : even++;
+        return min(odd, even);
+        /*
+        return min<int>(
+            count_if(begin(position), end(position), bind(modulus<int>(), placeholders::_1, 2)),
+            count_if(begin(position), end(position), not_fn(bind(modulus<int>(), placeholders::_1, 2)))
+            );
+         */
+    }
+};
