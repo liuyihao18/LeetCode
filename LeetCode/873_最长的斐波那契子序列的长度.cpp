79e38cbada4b196ba60b4d2353dfdded42bec488
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-07-09 10:09:50 +0800

    add 873

---
+++ LeetCode/873_最长的斐波那契子序列的长度.cpp
@@ -0,0 +1,34 @@
+// 873_最长的斐波那契子序列的长度.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& arr) {
+        int maxLen = 0;
+        for (size_t i = 0; i + 2 < arr.size(); i++) {
+            for (size_t j = i + 1; j + 1 < arr.size(); j++) {
+                int a = arr[i];
+                int b = arr[j];
+                int len = 2;
+                while (true) {
+                    int expected = a + b;
+                    auto iter = lower_bound(arr.begin(), arr.end(), expected);
+                    if (iter != arr.end() && *iter == expected) {
+                        a = b;
+                        b = expected;
+                        len++;
+                        continue;
+                    }
+                    else {
+                        break;
+                    }
+                }
+                if (len >= 3) {
+                    maxLen = max(maxLen, len);
+                }
+            }
+        }
+        return maxLen;
+    }
+};
