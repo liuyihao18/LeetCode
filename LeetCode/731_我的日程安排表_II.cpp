b8ec84e6d3c8f18bbfa3ea5abebefb649d7e7fb3
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-07-19 09:40:36 +0800

    add 731

---
+++ LeetCode/731_我的日程安排表_II.cpp
@@ -0,0 +1,36 @@
+// 731_我的日程安排表_II.cpp
+#include "custom.h"
+ustd
+
+class MyCalendarTwo {
+public:
+    MyCalendarTwo() {
+
+    }
+
+    bool book(int start, int end) {
+        // 如果和已有重叠区间重叠，则三次预定
+        for (auto& [l, r] : overlaps) {
+            if (l < end && start < r) {
+                return false;
+            }
+        }
+        // 求出和其他预订的重叠区间
+        for (auto& [l, r] : booked) {
+            if (l < end && start < r) {
+                overlaps.emplace_back(max(l, start), min(r, end));
+            }
+        }
+        booked.emplace_back(start, end);
+        return true;
+    }
+private:
+    vector<pair<int, int>> booked;
+    vector<pair<int, int>> overlaps;
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
