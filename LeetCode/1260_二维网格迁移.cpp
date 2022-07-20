242112ff3451b267d1e1e84ffba76887ac1cd08e
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-07-20 10:14:55 +0800

    add 1260

---
+++ LeetCode/1260_二维网格迁移.cpp
@@ -0,0 +1,28 @@
+// 1260_¶þÎ¬Íø¸ñÇ¨ÒÆ.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        vector<int> temp;
+        for (size_t i = 0; i < grid.size(); i++) {
+            for (size_t j = 0; j < grid[0].size(); j++) {
+                temp.push_back(grid[i][j]);
+            }
+        }
+        k = -(k % static_cast<int>(temp.size()));
+        for (size_t i = 0; i < grid.size(); i++) {
+            for (size_t j = 0; j < grid[0].size(); j++) {
+                if (k < 0) {
+                    grid[i][j] = temp[temp.size() + k];
+                }
+                else {
+                    grid[i][j] = temp[k];
+                }
+                k++;
+            }
+        }
+        return grid;
+    }
+};
