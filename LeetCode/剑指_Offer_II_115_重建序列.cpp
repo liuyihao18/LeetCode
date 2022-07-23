0942925ae9b8381a8a3578f3349cefa9086477a6
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-07-23 10:54:03 +0800

    add 115

---
+++ LeetCode/剑指_Offer_II_115_重建序列.cpp
@@ -0,0 +1,41 @@
+// 剑指_Offer_II_115_重建序列.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool sequenceReconstruction(vector<int>& nums, vector<vector<int>>& sequences) {
+        int n = static_cast<int>(nums.size());
+        vector<int> inDegree(n + 1);
+        vector<unordered_set<int>> graph(n + 1);
+        for (const auto& sequence : sequences) {
+            for (size_t i = 0; i + 1 < sequence.size(); i++) {
+                int u = sequence[i];
+                int v = sequence[i + 1];
+                if (!graph[u].count(v)) {
+                    graph[u].insert(v);
+                    inDegree[v]++;
+                }
+            }
+        }
+        queue<int> q;
+        for (int i = 1; i < n + 1; i++) {
+            if (inDegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        while (!q.empty()) {
+            if (q.size() > 1) {
+                return false;
+            }
+            int u = q.front();
+            q.pop();
+            for (auto v : graph[u]) {
+                if (--inDegree[v] == 0) {
+                    q.push(v);
+                }
+            }
+        }
+        return true;
+    }
+};
