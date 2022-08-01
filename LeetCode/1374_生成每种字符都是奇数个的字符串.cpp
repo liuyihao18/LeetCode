34e2faacb52359a38f3b59725903e0640d9f2c63
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-08-01 09:38:59 +0800

    add 1374

---
+++ LeetCode/1374_生成每种字符都是奇数个的字符串.cpp
@@ -0,0 +1,15 @@
+// 1374_生成每种字符都是奇数个的字符串.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    string generateTheString(int n) {
+        if (n & 1) {
+            return string(n, 'a');
+        }
+        else {
+            return string(n - 1, 'a') + 'b';
+        }
+    }
+};
