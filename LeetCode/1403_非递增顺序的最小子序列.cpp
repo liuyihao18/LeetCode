420f2dc3e7d6c81b78cac5b9a5fc49d9cda7f0a9
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-08-04 09:17:27 +0800

    add 1403

---
+++ LeetCode/1403_非递增顺序的最小子序列.cpp
@@ -0,0 +1,21 @@
+// 1403_非递增顺序的最小子序列.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> minSubsequence(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> result{ nums.back() };
+        nums.pop_back();
+        int sum = accumulate(result.begin(), result.end(), 0);
+        int remain = accumulate(nums.begin(), nums.end(), 0);
+        while (sum <= remain) {
+            result.push_back(nums.back());
+            sum += nums.back();
+            remain -= nums.back();
+            nums.pop_back();
+        }
+        return result;
+    }
+};
