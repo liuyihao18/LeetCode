29ac58d1b813e155b61699af979a3de817fcfdf3
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-08-13 09:29:13 +0800

    add 768

---
+++ LeetCode/768_最多能完成排序的块_II.cpp
@@ -0,0 +1,20 @@
+// 768_最多能完成排序的块_II.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        vector<int> sorted(arr);
+        sort(sorted.begin(), sorted.end());
+        unordered_map<int, int> cnt;
+        int result = 0;
+        for (size_t i = 0; i < sorted.size(); i++) {
+            int x = arr[i], y = sorted[i];
+            if (++cnt[x] == 0) cnt.erase(x);
+            if (--cnt[y] == 0) cnt.erase(y);
+            if (cnt.size() == 0) result++;
+        }
+        return result;
+    }
+};
