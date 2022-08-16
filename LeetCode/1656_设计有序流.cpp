cc72a8f565f79e992dce9ae5d0c72c95ffcedf52
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-08-16 09:33:57 +0800

    add 1656

---
+++ LeetCode/1656_设计有序流.cpp
@@ -0,0 +1,29 @@
+// 1656_设计有序流.cpp
+#include "custom.h"
+ustd
+
+class OrderedStream {
+    vector<string> v;
+    int capacity;
+    int ptr;
+public:
+    OrderedStream(int n) : v(n, ""), capacity(n), ptr(0) {
+
+    }
+
+    vector<string> insert(int idKey, string value) {
+        v[static_cast<size_t>(idKey) - 1] = value;
+        vector<string> result;
+        while (ptr < capacity && v[ptr] != "") {
+            result.push_back(v[ptr]);
+            ++ptr;
+        }
+        return result;
+    }
+};
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * OrderedStream* obj = new OrderedStream(n);
+ * vector<string> param_1 = obj->insert(idKey,value);
+ */
