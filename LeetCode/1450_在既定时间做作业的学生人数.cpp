4811f6d0f53ca51b00df175c95da3b5061bba4d0
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-08-19 08:45:47 +0800

    add 1450

---
+++ LeetCode/1450_在既定时间做作业的学生人数.cpp
@@ -0,0 +1,16 @@
+// 1450_在既定时间做作业的学生人数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int result = 0;
+        for (size_t i = 0; i < startTime.size(); i++) {
+            if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
+                result++;
+            }
+        }
+        return result;
+    }
+};
