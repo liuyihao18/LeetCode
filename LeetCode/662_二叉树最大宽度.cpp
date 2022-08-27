35569b0214b335560e55479db3476b780b00dbee
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-08-27 10:02:22 +0800

    add 662

---
+++ LeetCode/662_二叉树最大宽度.cpp
@@ -0,0 +1,43 @@
+// 662_二叉树最大宽度.cpp
+#include "custom.h"
+ustd
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        unsigned long long res = 1;
+        vector<pair<TreeNode*, unsigned long long>> arr;
+        arr.emplace_back(root, 1L);
+        while (!arr.empty()) {
+            vector<pair<TreeNode*, unsigned long long>> tmp;
+            for (auto& [node, index] : arr) {
+                if (node->left) {
+                    tmp.emplace_back(node->left, index * 2);
+                }
+                if (node->right) {
+                    tmp.emplace_back(node->right, index * 2 + 1);
+                }
+            }
+            res = max(res, arr.back().second - arr[0].second + 1);
+            arr = move(tmp);
+        }
+        return static_cast<int>(res);
+    }
+};
+/*
+作者：LeetCode - Solution
+链接：https ://leetcode.cn/problems/maximum-width-of-binary-tree/solution/er-cha-shu-zui-da-kuan-du-by-leetcode-so-9zp3/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
