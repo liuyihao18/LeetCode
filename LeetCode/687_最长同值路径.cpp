fa0478bcb707652eea747d5d9afd50af6ef6cdb4
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-09-02 10:46:43 +0800

    add 687

---
+++ LeetCode/687_最长同值路径.cpp
@@ -0,0 +1,39 @@
+// 687_最长同值路径.cpp
+#include "custom.h"
+ustd
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+  
+class Solution {
+    int result = 0;
+    int dfs(TreeNode* root) {
+        if (!root) return 0;
+        int leftPath = dfs(root->left);
+        int rightPath = dfs(root->right);
+        int leftSamePath = 0;
+        int rightSamePath = 0;
+        if (root->left && root->left->val == root->val) {
+            leftSamePath = leftPath + 1;
+        }
+        if (root->right && root->right->val == root->val) {
+            rightSamePath = rightPath + 1;
+        }
+        result = max(result, leftSamePath + rightSamePath);
+        return max(leftSamePath, rightSamePath);
+    }
+public:
+    int longestUnivaluePath(TreeNode* root) {
+        result = 0;
+        dfs(root);
+        return result;
+    }
+};
