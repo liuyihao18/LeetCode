73062053c74357eafdb547e30e98d2de6a7c08d5
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-09-03 09:59:04 +0800

    add 646

---
+++ LeetCode/646_最长数对链.cpp
@@ -0,0 +1,21 @@
+// 646_最长数对链.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(), pairs.end(), [](const vector<int>& p1, const vector<int>& p2) {
+            return p1[1] < p2[1];
+            });
+        int right = INT_MIN;
+        int result = 0;
+        for (const vector<int>& pair : pairs) {
+            if (pair[0] > right) {
+                result++;
+                right = pair[1];
+            }
+        }
+        return result;
+    }
+};
