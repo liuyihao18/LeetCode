e811ebbb8d5dcdbd7454ef63379bf10ddd1cb87b
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-09-04 10:12:45 +0800

    add 1582

---
+++ LeetCode/1582_二进制矩阵中的特殊位置.cpp
@@ -0,0 +1,31 @@
+// 1582_二进制矩阵中的特殊位置.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        size_t m = mat.size(), n = mat.front().size();
+        vector<int> rows(m, 0);
+        vector<int> cols(n, 0);
+        for (size_t i = 0; i < m; i++) {
+            for (size_t j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    rows[i]++;
+                    cols[j]++;
+                }
+            }
+        }
+        int result = 0;
+        for (size_t i = 0; i < m; i++) {
+            if (rows[i] == 1) {
+                for (size_t j = 0; j < n; j++) {
+                    if (cols[j] == 1 && mat[i][j] == 1) {
+                        result++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
