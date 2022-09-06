b884c736d493d27bbcb81c4c443a55c6004c986d
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-09-06 09:45:36 +0800

    add 828

---
+++ LeetCode/828_统计子串中的唯一字符.cpp
@@ -0,0 +1,23 @@
+// 828_统计子串中的唯一字符.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int uniqueLetterString(string s) {
+        int n = static_cast<int>(s.size());
+        unordered_map<char, vector<int>> um;
+        for (int i = 0; i < n; i++) {
+            um[s[i]].emplace_back(i);
+        }
+        int res = 0;
+        for (auto&& [_, arr] : um) {
+            arr.insert(arr.begin(), -1);
+            arr.emplace_back(n);
+            for (size_t i = 1; i + 1 < arr.size(); i++) {
+                res += (arr[i + 1] - arr[i]) * (arr[i] - arr[i - 1]);
+            }
+        }
+        return res;
+    }
+};
