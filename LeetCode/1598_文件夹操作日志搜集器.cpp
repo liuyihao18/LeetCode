c41e89ee68f17a07a64be5ac78d0cd1915476da1
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-09-09 09:43:26 +0800

    add 1598

---
+++ LeetCode/1598_文件夹操作日志搜集器.cpp
@@ -0,0 +1,16 @@
+// 1598_文件夹操作日志搜集器.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int result = 0;
+        for (const string& log : logs) {
+            if (log == "../") { if (result > 0) result--; }
+            else if (log == "./");
+            else result++;
+        }
+        return result;
+    }
+};
