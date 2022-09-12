6764ad7cb59a2e5ddf4962995cf5f1be964c979e
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-09-12 09:55:18 +0800

    add 1608

---
+++ LeetCode/1608_特殊数组的特征值.cpp
@@ -0,0 +1,26 @@
+// 1608_特殊数组的特征值.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int left = 0, right = static_cast<int>(nums.size());
+        while (left <= right) { // [left, right]
+            int mid = (left + right) / 2;
+            auto iter = lower_bound(nums.begin(), nums.end(), mid);
+            auto x = distance(iter, nums.end());
+            if (x < mid) {
+                right = mid - 1;
+            }
+            else if (x > mid) {
+                left = mid + 1;
+            }
+            else {
+                return x;
+            }
+        }
+        return -1;
+    }
+};
