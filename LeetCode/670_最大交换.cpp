c38ba60a5a04e192ad7a0d22a52f581ec12f6b64
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-09-13 12:02:12 +0800

    add 670

---
+++ LeetCode/670_最大交换.cpp
@@ -0,0 +1,35 @@
+// 670_×î´ó½»»».cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maximumSwap(int num) {
+        vector<int> digits;
+        while (num > 0) {
+            digits.push_back(num % 10);
+            num /= 10;
+        }
+        vector<int> maximum(digits.size(), 0);
+        int maxNum = 0;
+        for (size_t i = 0; i < digits.size(); i++) {
+            maximum[i] = maxNum;
+            maxNum = max(maxNum, digits[i]);
+        }
+        for (int i = static_cast<int>(digits.size()) - 1; i >= 0; i--) {
+            if (maximum[i] > digits[i]) {
+                for (size_t j = 0; j < digits.size(); j++) {
+                    if (digits[j] == maximum[i]) {
+                        swap(digits[j], digits[i]);
+                    }
+                }
+                break;
+            }
+        }
+        int result = 0;
+        for (int i = static_cast<int>(digits.size()) - 1; i >= 0; i--) {
+            result = result * 10 + digits[i];
+        }
+        return result;
+    }
+};
