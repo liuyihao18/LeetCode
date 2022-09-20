3657d2b39cdda21fafbd1f373d22bb2d5f4a593a
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-09-20 11:50:24 +0800

    add 698

---
+++ LeetCode/698_划分为k个相等的子集.cpp
@@ -0,0 +1,47 @@
+// 698_划分为k个相等的子集.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int all = accumulate(nums.begin(), nums.end(), 0);
+        if (all % k > 0) {
+            return false;
+        }
+        int per = all / k;
+        sort(nums.begin(), nums.end());
+        if (nums.back() > per) {
+            return false;
+        }
+        int n = static_cast<int>(nums.size());
+        vector<bool> dp(1LL << n, true);
+        function<bool(int, int)> dfs = [&](int s, int p)->bool {
+            if (s == 0) {
+                return true;
+            }
+            if (!dp[s]) {
+                return dp[s];
+            }
+            dp[s] = false;
+            for (int i = 0; i < n; i++) {
+                if (nums[i] + p > per) {
+                    break;
+                }
+                if ((s >> i) & 1) {
+                    if (dfs(s ^ (1 << i), (p + nums[i]) % per)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        };
+        return dfs((1 << n) - 1, 0);
+    }
+};
+/*
+作者：LeetCode - Solution
+链接：https ://leetcode.cn/problems/partition-to-k-equal-sum-subsets/solution/hua-fen-wei-kge-xiang-deng-de-zi-ji-by-l-v66o/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
