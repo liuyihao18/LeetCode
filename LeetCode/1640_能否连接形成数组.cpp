a559cfc2c8f199b20de8f9986f83a521c13c7874
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-09-22 09:41:02 +0800

    add 1640

---
+++ LeetCode/1640_能否连接形成数组.cpp
@@ -0,0 +1,29 @@
+// 1640_能否连接形成数组.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool canFormArray(vector<int>& arr, vector<vector<int>>& pieces) {
+        for (size_t i = 0; i < arr.size();) {
+            bool found = false;
+            for (auto& piece : pieces) {
+                if (arr[i] == piece[0]) {
+                    found = true;
+                    i++;
+                    for (size_t j = 1; j < piece.size(); j++) {
+                        if (arr[i] != piece[j]) {
+                            found = false;
+                        }
+                        i++;
+                    }
+                    break;
+                }
+            }
+            if (!found) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
