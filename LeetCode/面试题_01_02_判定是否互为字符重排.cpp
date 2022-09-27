73b7a60e8487940893b6916ad1c648c425edb5af
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-09-27 11:53:08 +0800

    add 01_02

---
+++ LeetCode/面试题_01_02_判定是否互为字符重排.cpp
@@ -0,0 +1,12 @@
+// 面试题_01_02_判定是否互为字符重排.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool CheckPermutation(string s1, string s2) {
+        sort(s1.begin(), s1.end());
+        sort(s2.begin(), s2.end());
+        return s1 == s2;
+    }
+};
