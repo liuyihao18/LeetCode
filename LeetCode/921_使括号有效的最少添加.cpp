6755d0008e2e05be59bb26d8177f5119173e42db
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-10-04 11:42:55 +0800

    add 921

---
+++ LeetCode/921_使括号有效的最少添加.cpp
@@ -0,0 +1,25 @@
+// 921_使括号有效的最少添加.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int cnt = 0;
+        int result = 0;
+        for (auto c : s) {
+            if (c == '(') {
+                if (cnt < 0) {
+                    result += -cnt;
+                    cnt = 0;
+                }
+                cnt++;
+            }
+            else {
+                cnt--;
+            }
+        }
+        result += cnt > 0 ? cnt : -cnt;
+        return result;
+    }
+};
