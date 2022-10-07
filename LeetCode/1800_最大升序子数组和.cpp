95f84f917dbd82addf6d402f2177c4b4728cc6f1
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-10-07 09:40:38 +0800

    add 1800

---
+++ LeetCode/1800_最大升序子数组和.cpp
@@ -0,0 +1,24 @@
+// 1800_最大升序子数组和.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int sum = 0;
+        int maxSum = INT_MIN;
+        int lastNum = INT_MIN;
+        for (auto num : nums) {
+            if (num > lastNum) {
+                sum += num;
+            }
+            else {
+                maxSum = max(maxSum, sum);
+                sum = num;
+            }
+            lastNum = num;
+        }
+        maxSum = max(maxSum, sum);
+        return maxSum;
+    }
+};
