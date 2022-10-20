6e3c74836e1a06ad712205a7e7ead3eccb81aaaa
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-10-20 10:32:28 +0800

    add 779

---
+++ LeetCode/779_第K个语法符号.cpp
@@ -0,0 +1,29 @@
+// 779_µÚK¸öÓï·¨·ûºÅ.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        int result = 0;
+        for (int i = 1; i < n; i++) {
+            if (result == 0) {
+                if ((k - 1) >> (n - 1 - i) & 1) {
+                    result = 1;
+                }
+                else {
+                    result = 0;
+                }
+            }
+            else {
+                if ((k - 1) >> (n - 1 - i) & 1) {
+                    result = 0;
+                }
+                else {
+                    result = 1;
+                }
+            }
+        }
+        return result;
+    }
+};
