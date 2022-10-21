58ffd12d9f0de6a24fd303b314fd417643ef41a5
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-10-21 19:59:53 +0800

    add 901

---
+++ LeetCode/901_股票价格跨度.cpp
@@ -0,0 +1,36 @@
+// 901_股票价格跨度.cpp
+#include "custom.h"
+ustd
+
+class StockSpanner {
+public:
+    StockSpanner() {
+        this->stk.emplace(-1, INT_MAX);
+        this->idx = -1;
+    }
+
+    int next(int price) {
+        idx++;
+        while (price >= stk.top().second) {
+            stk.pop();
+        }
+        int ret = idx - stk.top().first;
+        stk.emplace(idx, price);
+        return ret;
+    }
+
+private:
+    stack<pair<int, int>> stk;
+    int idx;
+};
+/*
+作者：LeetCode - Solution
+链接：https ://leetcode.cn/problems/online-stock-span/solution/gu-piao-jie-ge-kua-du-by-leetcode-soluti-5cm7/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
