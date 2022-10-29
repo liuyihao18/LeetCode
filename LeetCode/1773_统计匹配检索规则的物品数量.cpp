e251b78f25903115441fead6a73c8c68d8893fb7
刘怡豪 <1163122901@qq.com>
2022-10-29 12:54:00 +0800

    add 1773

---
+++ LeetCode/1773_统计匹配检索规则的物品数量.cpp
@@ -0,0 +1,20 @@
+// 1773_统计匹配检索规则的物品数量.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        size_t i = -1;
+        if (ruleKey == "type") i = 0;
+        else if (ruleKey == "color") i = 1;
+        else i = 2;
+        int result = 0;
+        for (const auto& item : items) {
+            if (item[i] == ruleValue) {
+                result++;
+            }
+        }
+        return result;
+    }
+};
