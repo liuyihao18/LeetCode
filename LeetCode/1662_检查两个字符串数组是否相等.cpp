55ffeca12e7f79c0a9741dad93f8b36ae082248d
刘怡豪 <1163122901@qq.com>
2022-11-01 12:52:46 +0800

    add 1662

---
+++ LeetCode/1662_检查两个字符串数组是否相等.cpp
@@ -0,0 +1,17 @@
+// 1662_检查两个字符串数组是否相等.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1, s2;
+        for (auto&& word : word1) {
+            s1 += word;
+        }
+        for (auto&& word : word2) {
+            s2 += word;
+        }
+        return s1 == s2;
+    }
+};
