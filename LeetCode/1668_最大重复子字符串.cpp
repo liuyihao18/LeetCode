36e1af368f78b8ad0eec325ed7f9e39d209d339e
刘怡豪 <1163122901@qq.com>
2022-11-03 10:19:19 +0800

    add 1668

---
+++ LeetCode/1668_最大重复子字符串.cpp
@@ -0,0 +1,19 @@
+// 1668_最大重复子字符串.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maxRepeating(string sequence, string word) {
+        for (int k = 1; k < 100; k++) {
+            string temp;
+            for (int i = 0; i < k; i++) {
+                temp += word;
+            }
+            if (sequence.find(temp) == string::npos) {
+                return k - 1;
+            }
+        }
+        return -1;
+    }
+};
