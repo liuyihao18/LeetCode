4c0a39ed6fd18da5fe06321fc4bcc39caa695e17
刘怡豪 <1163122901@qq.com>
2022-11-06 10:09:48 +0800

    add 1678

---
+++ LeetCode/1678_设计_Goal_解析器.cpp
@@ -0,0 +1,13 @@
+// 1678_Éè¼Æ_Goal_½âÎöÆ÷.cpp
+#include "custom.h"
+#include <regex>
+ustd
+
+class Solution {
+public:
+    string interpret(string command) {
+        command = regex_replace(command, regex("\\(\\)"), "o");
+        command = regex_replace(command, regex("\\(al\\)"), "al");
+        return command;
+    }
+};
