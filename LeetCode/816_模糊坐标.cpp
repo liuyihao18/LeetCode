8300e563dfae8fc1b2c6b3a50605c0ad8ecf43db
刘怡豪 <1163122901@qq.com>
2022-11-07 10:16:32 +0800

    add 816

---
+++ LeetCode/816_模糊坐标.cpp
@@ -0,0 +1,39 @@
+// 816_模糊坐标.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<string> getPos(string s) {
+        vector<string> pos;
+        if (s[0] != '0' || s == "0") pos.push_back(s);
+        for (int p = 1; p < s.size(); ++p) {
+            if ((p != 1 && s[0] == '0') || s.back() == '0') continue;
+            pos.push_back(s.substr(0, p) + "." + s.substr(p));
+        }
+        return pos;
+    }
+    vector<string> ambiguousCoordinates(string s) {
+        int n = static_cast<int>(s.size()) - 2;
+        vector<string> res;
+        s = s.substr(1, s.size() - 2);
+        for (int l = 1; l < n; ++l) {
+            vector<string> lt = getPos(s.substr(0, l));
+            if (lt.empty()) continue;
+            vector<string> rt = getPos(s.substr(l));
+            if (rt.empty()) continue;
+            for (auto& i : lt) {
+                for (auto& j : rt) {
+                    res.push_back("(" + i + ", " + j + ")");
+                }
+            }
+        }
+        return res;
+    }
+};
+/*
+作者：力扣官方题解
+链接：https ://leetcode.cn/problems/ambiguous-coordinates/solutions/1951931/mo-hu-zuo-biao-by-leetcode-solution-y1yz/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
