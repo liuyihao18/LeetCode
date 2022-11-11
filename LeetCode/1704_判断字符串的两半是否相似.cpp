28619b07948aa44910c0a25e81da2739765b5996
刘怡豪 <1163122901@qq.com>
2022-11-11 10:44:29 +0800

    add 1704

---
+++ LeetCode/1704_判断字符串的两半是否相似.cpp
@@ -0,0 +1,26 @@
+// 1704_判断字符串的两半是否相似.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'; 
+    }
+public:
+    bool halvesAreAlike(string s) {
+        int cnt = 0;
+        for (size_t i = 0; i < s.size() / 2; i++) {
+            s[i] = tolower(s[i]);
+            if (isVowel(s[i])) {
+                cnt++;
+            }
+        }
+        for (size_t i = s.size() / 2; i < s.size(); i++) {
+            s[i] = tolower(s[i]);
+            if (isVowel(s[i])) {
+                cnt--;
+            }
+        }
+        return cnt == 0;
+    }
+};
