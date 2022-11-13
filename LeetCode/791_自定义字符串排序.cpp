910fc9927a2851ad4e423ccb86da82bf64aadc5d
刘怡豪 <1163122901@qq.com>
2022-11-13 11:09:30 +0800

    add 791

---
+++ LeetCode/791_自定义字符串排序.cpp
@@ -0,0 +1,30 @@
+// 791_×Ô¶¨Òå×Ö·û´®ÅÅÐò.cpp
+#include "custom.h"
+ustd
+
+#define INDEX(c) static_cast<size_t>(c) - 'a'
+#define CHAR(i) static_cast<char>(i) + 'a'
+
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int> cnt(26, 0);
+        for (auto c : s) {
+            cnt[INDEX(c)]++;
+        }
+        string result;
+        for (auto c : order) {
+            while (cnt[INDEX(c)] > 0) {
+                result += c;
+                cnt[INDEX(c)]--;
+            }
+        }
+        for (size_t i = 0; i < cnt.size(); i++) {
+            while (cnt[i] > 0) {
+                result += CHAR(i);
+                cnt[i]--;
+            }
+        }
+        return result;
+    }
+};
