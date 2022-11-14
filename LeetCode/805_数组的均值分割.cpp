0f51c54fd72038a6802d97ced2eba3da8f777b6f
刘怡豪 <1163122901@qq.com>
2022-11-14 10:19:12 +0800

    add 805

---
+++ LeetCode/805_数组的均值分割.cpp
@@ -0,0 +1,52 @@
+// 805_数组的均值分割.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool splitArraySameAverage(vector<int>& nums) {
+        int n = static_cast<int>(nums.size()), m = n / 2;
+        if (n == 1) {
+            return false;
+        }
+
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        for (int& x : nums) {
+            x = x * n - sum;
+        }
+
+        unordered_set<int> left;
+        for (int i = 1; i < (1 << m); i++) {
+            int tot = 0;
+            for (int j = 0; j < m; j++) {
+                if (i & (1 << j)) {
+                    tot += nums[j];
+                }
+            }
+            if (tot == 0) {
+                return true;
+            }
+            left.emplace(tot);
+        }
+
+        int rsum = accumulate(nums.begin() + m, nums.end(), 0);
+        for (int i = 1; i < (1 << (n - m)); i++) {
+            int tot = 0;
+            for (int j = m; j < n; j++) {
+                if (i & (1 << (j - m))) {
+                    tot += nums[j];
+                }
+            }
+            if (tot == 0 || (rsum != tot && left.count(-tot))) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+/*
+作者：力扣官方题解
+链接：https ://leetcode.cn/problems/split-array-with-same-average/solutions/1966163/shu-zu-de-jun-zhi-fen-ge-by-leetcode-sol-f630/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
