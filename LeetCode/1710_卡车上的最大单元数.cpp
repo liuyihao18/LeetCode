f940b4f94d7daf9b321ebdad218badba48ee2878
刘怡豪 <1163122901@qq.com>
2022-11-15 13:26:16 +0800

    add 1710

---
+++ LeetCode/1710_卡车上的最大单元数.cpp
@@ -0,0 +1,26 @@
+// 1710_卡车上的最大单元数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(), boxTypes.end(), [](const vector<int>& boxType1, const vector<int>& boxType2) {
+            return boxType1[1] > boxType2[1];
+            });
+        int result = 0;
+        size_t index = 0;
+        while (truckSize > 0) {
+            while (index < boxTypes.size() && boxTypes[index][0] <= 0) {
+                index++;
+            }
+            if (index >= boxTypes.size()) {
+                break;
+            }
+            result += boxTypes[index][1];
+            boxTypes[index][0]--;
+            truckSize--;
+        }
+        return result;
+    }
+};
