ec42894b35d088a763e450e306398868e3b2d7f8
刘怡豪 <1163122901@qq.com>
2022-11-19 10:28:07 +0800

    add 1732

---
+++ LeetCode/1732_找到最高海拔.cpp
@@ -0,0 +1,16 @@
+// 1732_找到最高海拔.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int result = 0;
+        int curr = 0;
+        for (auto g : gain) {
+            curr += g;
+            result = max(result, curr);
+        }
+        return result;
+    }
+};
