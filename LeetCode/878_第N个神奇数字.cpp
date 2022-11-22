438909cdceb783aea109b6b6da1430f53829dafb
刘怡豪 <1163122901@qq.com>
2022-11-22 13:40:40 +0800

    add 878

---
+++ LeetCode/878_第N个神奇数字.cpp
@@ -0,0 +1,46 @@
+// 878_第N个神奇数字.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    int gcd(int a, int b) {
+        while (b > 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+    int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+    long long kth(long long num, long long a, long long b, long long c, bool& ok) {
+        ok = (num % a == 0 || num % b == 0);
+        if (!ok) {
+            num = max(num / a * a, num / b * b);
+        }
+        return num / a + num / b - num / c;
+    }
+public:
+    int nthMagicalNumber(int n, int a, int b) {
+        long long remainder = 1000000007;
+        long long c = lcm(a, b);
+        long long right = static_cast<long long>(max(a, b)) * n + 1;
+        long long left = static_cast<long long>(min(a, b));
+        while (left < right) {
+            long long mid = (left + right) / 2;
+            bool ok;
+            long long k = kth(mid, a, b, c, ok);
+            if (k == n && ok) {
+                return static_cast<int>(mid % remainder);
+            }
+            else if (k < n) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return -1;
+    }
+};
