3ff37a43359df310b5c50eec2746e367805e75f2
刘怡豪 <1163122901@qq.com>
2022-11-23 10:37:21 +0800

    add 1742

---
+++ LeetCode/1742_盒子中小球的最大数量.cpp
@@ -0,0 +1,26 @@
+// 1742_盒子中小球的最大数量.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    int digitSum(int num) {
+        int result = 0;
+        while (num > 0) {
+            result += num % 10;
+            num /= 10;
+        }
+        return result;
+    }
+public:
+    int countBalls(int lowLimit, int highLimit) {
+        map<int, int> m;
+        for (int i = lowLimit; i <= highLimit; i++) {
+            m[digitSum(i)]++;
+        }
+        int maxCount = 0;
+        for (const auto& p : m) {
+            maxCount = max(maxCount, p.second);
+        }
+        return maxCount;
+    }
+};
