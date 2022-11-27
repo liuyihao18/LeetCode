170e2f9271187fae6aa228e473bc628f4beacef8
刘怡豪 <1163122901@qq.com>
2022-11-27 10:39:09 +0800

    add 1752

---
+++ LeetCode/1752_检查数组是否经排序和轮转得到.cpp
@@ -0,0 +1,23 @@
+// 1752_检查数组是否经排序和轮转得到.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        vector<int> origin = nums;
+        sort(origin.begin(), origin.end());
+        for (size_t offset = 0; offset < nums.size(); offset++) {
+            size_t i = 0;
+            for (; i < nums.size(); i++) {
+                if (nums[i] != origin[(i + offset) % nums.size()]) {
+                    break;
+                }
+            }
+            if (i == nums.size()) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
