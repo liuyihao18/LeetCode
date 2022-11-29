9a2613354660bec658a65e23f6ac8a793750c16f
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-11-29 19:07:14 +0800

    add 1758

---
+++ LeetCode/1758_生成交替二进制字符串的最少操作数.cpp
@@ -0,0 +1,19 @@
+// 1758_生成交替二进制字符串的最少操作数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minOperations(string s) {
+        int result = 0;
+        for (size_t i = 0; i < s.size(); i++) {
+            if (i & 1 && s[i] != '0') {
+                result++;
+            }
+            else if (!(i & 1) && s[i] != '1') {
+                result++;
+            }
+        }
+        return min(result, static_cast<int>(s.size()) - result);
+    }
+};
