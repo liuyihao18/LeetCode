0ebf07884d79f69afbe6a3b1d9b95bc537265a8e
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-02 17:45:50 +0800

    add 1769

---
+++ LeetCode/1769_移动所有球到每个盒子所需的最小操作数.cpp
@@ -0,0 +1,18 @@
+// 1769_移动所有球到每个盒子所需的最小操作数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        vector<int> answer(boxes.size(), 0);
+        for (int i = 0; i < static_cast<int>(boxes.size()); i++) {
+            int temp = 0;
+            for (int j = 0; j < static_cast<int>(boxes.size()); j++) {
+                temp += (boxes[j] - '0') * abs(j - i);
+            }
+            answer[i] = temp;
+        }
+        return answer;
+    }
+};
