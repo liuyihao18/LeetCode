58e74b7164fbb1015b985d7ed4495fe7f704a623
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-06 11:39:24 +0800

    add 1805

---
+++ LeetCode/1805_字符串中不同整数的数目.cpp
@@ -0,0 +1,26 @@
+// 1805_字符串中不同整数的数目.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int numDifferentIntegers(string word) {
+        for (auto& c : word) {
+            if (!isdigit(c)) {
+                c = ' ';
+            }
+        }
+        istringstream is(word);
+        set<string> s;
+        string num;
+        while (is >> num) {
+            while (num.size() > 1 && *num.begin() == '0') {
+                num.erase(num.begin());
+            }
+            if (num.size() > 0) {
+                s.insert(num);
+            }
+        }
+        return static_cast<int>(s.size());
+    }
+};
