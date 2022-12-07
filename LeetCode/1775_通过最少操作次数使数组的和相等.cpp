5875a13fe3331efb10a1e4ad06dd681d506295b1
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-07 10:08:54 +0800

    add 1775

---
+++ LeetCode/1775_通过最少操作次数使数组的和相等.cpp
@@ -0,0 +1,51 @@
+// 1775_通过最少操作次数使数组的和相等.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int help(vector<int>& h1, vector<int>& h2, int diff) {
+        vector<int> h(7, 0);
+        for (int i = 1; i < 7; ++i) {
+            h[6 - i] += h1[i];
+            h[i - 1] += h2[i];
+        }
+        int res = 0;
+        for (int i = 5; i && diff > 0; --i) {
+            int t = min((diff + i - 1) / i, h[i]);
+            res += t;
+            diff -= t * i;
+        }
+        return res;
+    }
+
+    int minOperations(vector<int>& nums1, vector<int>& nums2) {
+        int n = static_cast<int>(nums1.size()), m = static_cast<int>(nums2.size());
+        if (6 * n < m || 6 * m < n) {
+            return -1;
+        }
+        vector<int> cnt1(7, 0), cnt2(7, 0);
+        int diff = 0;
+        for (auto& i : nums1) {
+            ++cnt1[i];
+            diff += i;
+        }
+        for (auto& i : nums2) {
+            ++cnt2[i];
+            diff -= i;
+        }
+        if (!diff) {
+            return 0;
+        }
+        if (diff > 0) {
+            return help(cnt2, cnt1, diff);
+        }
+        return help(cnt1, cnt2, -diff);
+    }
+};
+/*
+作者：力扣官方题解
+链接：https ://leetcode.cn/problems/equal-sum-arrays-with-minimum-number-of-operations/solutions/2008878/tong-guo-zui-shao-cao-zuo-ci-shu-shi-shu-e3o1/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
