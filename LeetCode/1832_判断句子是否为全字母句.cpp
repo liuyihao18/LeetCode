0e06b58895431cef31eabb38f6a9ff88bbe3d32a
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-13 11:30:44 +0800

    add 1832

---
+++ LeetCode/1832_判断句子是否为全字母句.cpp
@@ -0,0 +1,14 @@
+// 1832_判断句子是否为全字母句
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int> cnts(26, 0);
+        for (auto c : sentence) {
+            cnts[static_cast<size_t>(c - 'a')]++;
+        }
+        return all_of(cnts.begin(), cnts.end(), [](int cnt) { return cnt > 0; });
+    }
+};
