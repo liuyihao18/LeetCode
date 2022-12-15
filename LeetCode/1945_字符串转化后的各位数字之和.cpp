cd0578d352770fbcdc0ff2ecc0065662eb71420a
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-15 10:30:45 +0800

    add 1945

---
+++ LeetCode/1945_字符串转化后的各位数字之和.cpp
@@ -0,0 +1,23 @@
+// 1945_字符串转化后的各位数字之和.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string temp;
+        for (auto c : s) {
+            temp += to_string(c - 'a' + 1);
+        }
+        int number = 0;
+        while (k > 0) {
+            number = 0;
+            for (auto c : temp) {
+                number += c - '0';
+            }
+            temp = to_string(number);
+            k--;
+        }
+        return number;
+    }
+};
