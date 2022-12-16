6531026b958ff56128e6e99388518fed5d73b6c6
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-16 11:22:25 +0800

    add 1785

---
+++ LeetCode/1785_构成特定和需要添加的最少元素.cpp
@@ -0,0 +1,13 @@
+// 1785_构成特定和需要添加的最少元素.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minElements(vector<int>& nums, int limit, int goal) {
+        long long sum = accumulate(nums.begin(), nums.end(), 0LL);
+        long long diff = abs(sum - goal);
+        int result = static_cast<int>((diff + limit - 1) / limit);
+        return result;
+    }
+};
