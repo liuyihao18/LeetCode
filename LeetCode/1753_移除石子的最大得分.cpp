21e9862746884a786bc8b6c9cd176ecd28d8b00b
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-21 10:50:35 +0800

    add 1753

---
+++ LeetCode/1753_移除石子的最大得分.cpp
@@ -0,0 +1,28 @@
+// 1753_移除石子的最大得分.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        int stones[] = {a, b, c};
+        sort(begin(stones), end(stones));
+        int result = 0;
+        a = stones[0], b = stones[1], c = stones[2];
+        if (a < c - b) {
+            result += a;
+            c -= a;
+        }
+        else {
+            result += c - b;
+            a -= (c - b);
+            c = b;
+            result += a;
+            c -= a / 2;
+            b -= a - a / 2;
+            a -= a;
+        }
+        result += b;
+        return result;
+    }
+};
