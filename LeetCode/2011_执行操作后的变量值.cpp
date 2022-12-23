7dc6a2ebb9faf9704d6889a28746f40ba0d7c4e1
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-23 11:01:16 +0800

    add 2011

---
+++ LeetCode/2011_执行操作后的变量值.cpp
@@ -0,0 +1,19 @@
+// 2011_执行操作后的变量值.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for (const auto& operation : operations) {
+            if (operation == "X++" || operation == "++X") {
+                x++;
+            }
+            else {
+                x--;
+            }
+        }
+        return x;
+    }
+};
