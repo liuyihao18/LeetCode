5d909309f94e8e6db8aaf496e46a59dc8c27d788
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-26 09:28:47 +0800

    add 1759

---
+++ LeetCode/1759_统计同构子字符串的数目.cpp
@@ -0,0 +1,29 @@
+// 1759_统计同构子字符串的数目.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int countHomogenous(string s) {
+        int i = 0, j = 0, n = static_cast<int>(s.size());
+        int result = 0;
+        int temp = 0;
+        const int remainder = (int)1e9 + 7;
+        while (j < s.size()) {
+            if (s[j] == s[i]) {
+                temp += (j - i + 1) % remainder;
+                temp %= remainder;
+                j++;
+            }
+            else {
+                result += temp;
+                result %= remainder;
+                temp = 0;
+                i = j;
+            }
+        }
+        result += temp;
+        result %= remainder;
+        return result;
+    }
+};
