d5f10727ebe7473f56878fc770743f72d6240ae0
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-27 10:25:03 +0800

    add 2027

---
+++ LeetCode/2027_转换字符串的最少操作次数.cpp
@@ -0,0 +1,23 @@
+// 2027_转换字符串的最少操作次数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int result = 0;
+        for (size_t i = 0; i < s.size();) {
+            if (s[i] == 'X') {
+                for (size_t j = 0; j < 3 && i + j < s.size(); j++) {
+                    s[i + j] = 'O';
+                }
+                i += 3;
+                result++;
+            }
+            else {
+                i++;
+            }
+        }
+        return result;
+    }
+};
