b769044fa665d7785c4f2c6a634f9c76ec7b7712
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-28 11:14:19 +0800

    add 1750

---
+++ LeetCode/1750_删除字符串两端相同字符后的最短长度.cpp
@@ -0,0 +1,23 @@
+// 1750_删除字符串两端相同字符后的最短长度.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minimumLength(string s) {
+        size_t i = 0, j = s.size() - 1;
+        while (true) {
+            if (j - i < 1 || s[i] != s[j]) {
+                break;
+            }
+            auto c = s[i];
+            while (j - i + 1 > 0 && s[i] == c) {
+                i++;
+            }
+            while (j - i + 1 > 0 && s[j] == c) {
+                j--;
+            }
+        }
+        return j - i + 1;
+    }
+};
