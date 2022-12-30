4c38d39ec401a29fb219647cb45ac7ab9de7b4dc
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-30 10:17:14 +0800

    add 2032

---
+++ LeetCode/2032_至少在两个数组中出现的值.cpp
@@ -0,0 +1,38 @@
+// 2032_至少在两个数组中出现的值.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
+        map<int, size_t> cnt;
+        set<int> temp;
+        for (auto num : nums1) {
+            if (!temp.count(num)){
+                temp.insert(num);
+                cnt[num]++;
+            }            
+        }
+        temp.clear();
+        for (auto num : nums2) {
+            if (!temp.count(num)) {
+                temp.insert(num);
+                cnt[num]++;
+            }
+        }
+        temp.clear();
+        for (auto num : nums3) {
+            if (!temp.count(num)) {
+                temp.insert(num);
+                cnt[num]++;
+            }
+        }
+        vector<int> result;
+        for (auto& [key, val] : cnt) {
+            if (val > 1) {
+                result.push_back(key);
+            }
+        }
+        return result;
+    }
+};
