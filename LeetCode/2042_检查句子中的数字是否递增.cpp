cd08c8f39aa534054f199ba36f7bd97ca0260a9a
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-01-03 10:29:50 +0800

    add 2042

---
+++ LeetCode/2042_检查句子中的数字是否递增.cpp
@@ -0,0 +1,28 @@
+// 2042_检查句子中的数字是否递增.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool areNumbersAscending(string s) {
+        s += ' ';
+        string temp;
+        int lastNumber = -1;
+        for (auto c : s) {
+            if (c != ' ') {
+                temp += c;
+            }
+            else {
+                if (temp.size() > 0 && isdigit(temp[0])) {
+                    int number = stoi(temp);
+                    if (number <= lastNumber) {
+                        return false;
+                    }
+                    lastNumber = number;
+                }
+                temp.clear();
+            }
+        }
+        return true;
+    }
+};
