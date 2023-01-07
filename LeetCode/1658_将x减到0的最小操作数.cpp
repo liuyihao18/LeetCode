1acc265ffa60276fb5b4275cd8d0447412c97c5e
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-01-07 10:52:47 +0800

    add 1658

---
+++ LeetCode/1658_将x减到0的最小操作数.cpp
@@ -0,0 +1,40 @@
+// 1658_将x减到0的最小操作数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int n = static_cast<int>(nums.size());
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        if (sum < x) {
+            return -1;
+        }
+
+        int right = 0;
+        int lsum = 0, rsum = sum;
+        int ans = n + 1;
+
+        for (int left = -1; left < n; ++left) {
+            if (left != -1) {
+                lsum += nums[left];
+            }
+            while (right < n && lsum + rsum > x) {
+                rsum -= nums[right];
+                ++right;
+            }
+            if (lsum + rsum == x) {
+                ans = min(ans, (left + 1) + (n - right));
+            }
+        }
+
+        return ans > n ? -1 : ans;
+    }
+};
+/*
+作者：力扣官方题解
+链接：https ://leetcode.cn/problems/minimum-operations-to-reduce-x-to-zero/solutions/2047253/jiang-x-jian-dao-0-de-zui-xiao-cao-zuo-s-hl7u/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
