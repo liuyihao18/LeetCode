8ac2d10239afcb17040f1bed23409c959c48500d
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-01-10 11:01:14 +0800

    add 753

---
+++ LeetCode/753_破解保险箱.cpp
@@ -0,0 +1,37 @@
+// 753_破解保险箱.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+private:
+    unordered_set<int> seen;
+    string ans;
+    int highest;
+    int k;
+
+public:
+    void dfs(int node) {
+        for (int x = 0; x < k; ++x) {
+            int nei = node * 10 + x;
+            if (!seen.count(nei)) {
+                seen.insert(nei);
+                dfs(nei % highest);
+                ans += (x + '0');
+            }
+        }
+    }
+
+    string crackSafe(int n, int k) {
+        highest = static_cast<int>(pow(10, n - 1));
+        this->k = k;
+        dfs(0);
+        ans += string(n - 1, '0');
+        return ans;
+    }
+};
+/*
+作者：力扣官方题解
+链接：https ://leetcode.cn/problems/cracking-the-safe/solutions/393529/po-jie-bao-xian-xiang-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
