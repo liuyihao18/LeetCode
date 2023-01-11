bdf740ea05ddc83de025ac7dc8eae97f8f9da210
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-01-11 10:54:12 +0800

    add 2283

---
+++ LeetCode/2283_判断一个数的数字计数是否等于数位的值.cpp
@@ -0,0 +1,18 @@
+// 2283_判断一个数的数字计数是否等于数位的值.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool digitCount(string num) {
+        vector<int> counts(10, 0);
+        for (auto c : num) {
+            counts[c - '0']++;
+        }
+        string temp;
+        for (auto count : counts) {
+            temp += count + '0';
+        }
+        return num == temp.substr(0, num.size());
+    }
+};
