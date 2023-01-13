b07779748032329d78ecb54665fe6ed9fc52d441
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-01-13 10:34:15 +0800

    add 2287

---
+++ LeetCode/2287_重排字符形成目标字符串.cpp
@@ -0,0 +1,22 @@
+// 2287_重排字符形成目标字符串.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int rearrangeCharacters(string s, string target) {
+        map<char, int> m;
+        for (auto c : s) {
+            m[c]++;
+        }
+        map<char, int> t;
+        for (auto c : target) {
+            t[c]++;
+        }
+        int result = INT_MAX;
+        for (pair<char, int> p : t) {
+            result = min(result, m[p.first] / p.second);
+        }
+        return result;
+    }
+};
