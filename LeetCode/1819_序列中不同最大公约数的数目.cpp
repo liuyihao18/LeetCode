15612652a90b9f044134e7233b4ec639ad83c6b0
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-01-14 10:29:33 +0800

    add 1819

---
+++ LeetCode/1819_序列中不同最大公约数的数目.cpp
@@ -0,0 +1,47 @@
+// 1819_序列中不同最大公约数的数目.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    int gcd(int a, int b) {
+        while (b > 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+public:
+    int countDifferentSubsequenceGCDs(vector<int>& nums) {
+        int maxVal = *max_element(nums.begin(), nums.end());
+        vector<bool> occured(maxVal + 1, false);
+        for (int num : nums) {
+            occured[num] = true;
+        }
+        int ans = 0;
+        for (int i = 1; i <= maxVal; i++) {
+            int subGcd = 0;
+            for (int j = i; j <= maxVal; j += i) {
+                if (occured[j]) {
+                    if (subGcd == 0) {
+                        subGcd = j;
+                    }
+                    else {
+                        subGcd = gcd(subGcd, j);
+                    }
+                    if (subGcd == i) {
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+/*
+作者：力扣官方题解
+链接：https ://leetcode.cn/problems/number-of-different-subsequences-gcds/solutions/2060230/xu-lie-zhong-bu-tong-zui-da-gong-yue-shu-ha1j/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
