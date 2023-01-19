5b7beb07e6c6b75856092753bb638743bb7244e4
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-01-19 10:04:51 +0800

    add 2299

---
+++ LeetCode/2299_强密码检验器_II.cpp
@@ -0,0 +1,39 @@
+// 2299_«ø√‹¬ÎºÏ—È∆˜_II.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool strongPasswordCheckerII(string password) {
+        if (password.size() < 8) {
+            return false;
+        }
+        if (all_of(password.begin(), password.end(), [](char c) {
+            return !(c <= 'z' && c >= 'a');
+            })) {
+            return false;
+        }
+        if (all_of(password.begin(), password.end(), [](char c) {
+            return !(c <= 'Z' && c >= 'A');
+            })) {
+            return false;
+        }
+        if (all_of(password.begin(), password.end(), [](char c) {
+            return !(c <= '9' && c >= '0');
+            })) {
+            return false;
+        }
+        string symbol = "!@#$%^&*()-+";
+        if (all_of(password.begin(), password.end(), [&](char c) {
+            return symbol.find(c) == string::npos;
+            })) {
+            return false;
+        }
+        for (size_t i = 0; i + 1 < password.size(); i++) {
+            if (password[i] == password[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
