120a5a9c441b9f748313c0c393eb55bf2cd955ee
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-01-29 11:16:02 +0800

    add 2315

---
+++ LeetCode/2315_统计星号.cpp
@@ -0,0 +1,20 @@
+// 2315_Í³¼ÆÐÇºÅ.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int countAsterisks(string s) {
+        bool in = false;
+        int result = 0;
+        for (auto c : s) {
+            if (c == '|') {
+                in = !in;
+            }
+            else if (c == '*' && !in) {
+                result++;
+            }
+        }
+        return result;
+    }
+};
