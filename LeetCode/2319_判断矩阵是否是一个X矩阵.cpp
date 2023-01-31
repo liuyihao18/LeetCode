fa00acbe1d0a63ceb34ff3a84d85c88d6f4558a1
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-01-31 10:06:31 +0800

    add 2319

---
+++ LeetCode/2319_判断矩阵是否是一个X矩阵.cpp
@@ -0,0 +1,20 @@
+// 2319_判断矩阵是否是一个X矩阵.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        size_t n = grid.size();
+        for (size_t i = 0; i < n; i++) {
+            for (size_t j = 0; j < n; j++) {
+                if ((i == j || i + j + 1 == n) && grid[i][j] == 0) 
+                    return false;
+                if (!(i == j || i + j + 1 == n) && grid[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
