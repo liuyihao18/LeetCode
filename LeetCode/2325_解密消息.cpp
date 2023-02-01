9e8e3d4fab350d1359fce8065905b8e87b5c64f4
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-02-01 10:03:45 +0800

    add 2325

---
+++ LeetCode/2325_解密消息.cpp
@@ -0,0 +1,19 @@
+// 2325_½âÃÜÏûÏ¢.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        vector<char> table(26, 0);
+        char target = 'a';
+        for (size_t i = 0; i < key.size() && target <= 'z'; i++) {
+            if (key[i] != ' ' && table[key[i] - 'a'] == 0) 
+                table[key[i] - 'a'] = target++;
+        }
+        for (auto& c : message) {
+            if (c != ' ') c = table[c - 'a'];
+        }
+        return message;
+    }
+};
