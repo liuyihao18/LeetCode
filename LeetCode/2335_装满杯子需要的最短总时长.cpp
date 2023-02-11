9ec117be716a948024b5d8d7ba5fbec89f078ba7
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-02-11 11:16:32 +0800

    add 2335

---
+++ LeetCode/2335_装满杯子需要的最短总时长.cpp
@@ -0,0 +1,23 @@
+// 2335_装满杯子需要的最短总时长.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int fillCups(vector<int>& amount) {
+        int result = 0;
+        sort(amount.begin(), amount.end());
+        while (amount[2] > 0) {
+            amount[2]--;
+            if (amount[0] > 0) {
+                amount[0]--;
+            }
+            else if (amount[1] > 0) {
+                amount[1]--;
+            }
+            result++;
+            sort(amount.begin(), amount.end());
+        }
+        return result;
+    }
+};
