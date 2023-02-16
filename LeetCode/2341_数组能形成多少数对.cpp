66466e8b93aea3efc0104a7eb89c884d0d8087ba
刘怡豪 <1163122901@qq.com>
2023-02-16 21:31:13 +0800

    add 2341

---
+++ LeetCode/2341_数组能形成多少数对.cpp
@@ -0,0 +1,19 @@
+// 2341_数组能形成多少数对.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> numberOfPairs(vector<int>& nums) {
+        map<int, int> m;
+        for (auto num : nums) {
+            m[num]++;
+        }
+        int x = 0, y = 0;
+        for (auto& [_, c] : m) {
+            x += c / 2;
+            y += c % 2;
+        }
+        return { x,y };
+    }
+};
