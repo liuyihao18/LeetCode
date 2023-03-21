16b2739ab843ff6a5c2f68a0fd98b05f00b7235e
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-03-21 15:10:03 +0800

    add 2469

---
+++ LeetCode/2469_温度转换.cpp
@@ -0,0 +1,13 @@
+// 2469_ÎÂ¶È×ª»».cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        return {
+            celsius + 273.15,
+            celsius * 1.80 + 32.00
+        };
+    }
+};
