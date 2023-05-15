ea20d43438237f8b23aa38ceadeb718ec50fc381
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-05-15 11:45:49 +0800

    update 1072

---
+++ LeetCode/1072_按列翻转得到最大值等行数.cpp
@@ -0,0 +1,22 @@
+// 1072_按列翻转得到最大值等行数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        unordered_map<string, int> um;
+        size_t m = matrix.size();
+        size_t n = matrix[0].size();
+        for (size_t i = 0; i < m; i++) {
+            string temp;
+            for (size_t j = 0; j < n; j++) {
+                temp += '0' + (matrix[i][j] ^ matrix[i][0]);
+            }
+            um[temp]++;
+        }
+        return max_element(um.begin(), um.end(), [](const pair<string, int>& p1, const pair<string, int>& p2) {
+            return p1.second < p2.second;
+            })->second;
+    }
+};
