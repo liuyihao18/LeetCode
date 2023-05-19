3860540cfb2b9769fb60bcbc5a3f5a2c0ad17fc4
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-05-19 11:12:10 +0800

    add 1079

---
+++ LeetCode/1079_活字印刷.cpp
@@ -0,0 +1,39 @@
+// 1079_»î×ÖÓ¡Ë¢.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    int A(int m, int n) {
+        int result = 1;
+        for (int i = 0; i < n; i++, m--) {
+            result *= m;
+        }
+        return result;
+    }
+public:
+    int numTilePossibilities(string tiles) {
+        int result = 0;
+        set<map<char, int>> visited;
+        size_t l = tiles.length();
+        for (size_t s = 1; s < (static_cast<size_t>(1) << l); s++) {
+            map<char, int> visit;
+            int tl = 0;
+            for (size_t i = 0; i < l; i++) {
+                if (s >> i & 1) {
+                    visit[tiles[i]]++;
+                    tl++;
+                }
+            }
+            if (!visited.count(visit)) {
+                visited.insert(visit);
+                int temp = 1;
+                temp *= A(tl, tl);
+                for (auto& [_, count] : visit) {
+                    temp /= A(count, count);
+                }
+                result += temp;
+            }
+        }
+        return result;
+    }
+};
