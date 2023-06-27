3079cf4db8afdb98d768927b3fe420b69fda18b6
刘怡豪 <1163122901@qq.com>
2023-06-27 13:45:55 +0800

    add 1186

---
+++ LeetCode/1186_删除一次得到子数组最大和.cpp
@@ -0,0 +1,16 @@
+// 1186_删除一次得到子数组最大和.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maximumSum(vector<int>& arr) {
+        int dp0 = arr[0], dp1 = 0, res = arr[0];
+        for (size_t i = 1; i < arr.size(); i++) {
+            dp1 = max(dp0, dp1 + arr[i]);
+            dp0 = max(dp0, 0) + arr[i];
+            res = max(res, max(dp0, dp1));
+        }
+        return res;
+    }
+};
