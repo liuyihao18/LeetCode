ce0116efe6050b44ab1af0dd50a8cfcebb88c073
刘怡豪 <1163122901@qq.com>
2023-07-04 19:41:26 +0800

    add 2679

---
+++ LeetCode/2679_矩阵中的和.cpp
@@ -0,0 +1,22 @@
+// 2679_¾ØÕóÖÐµÄºÍ.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int matrixSum(vector<vector<int>>& nums) {
+        size_t m = nums.size(), n = nums.front().size();
+        for (size_t i = 0; i < m; i++) {
+            sort(nums[i].begin(), nums[i].end());
+        }
+        int result = 0;
+        for (size_t j = 0; j < n; j++) {
+            int maximum = INT_MIN;
+            for (size_t i = 0; i < m; i++) {
+                maximum = max(maximum, nums[i][j]);
+            }
+            result += maximum;
+        }
+        return result;
+    }
+};
