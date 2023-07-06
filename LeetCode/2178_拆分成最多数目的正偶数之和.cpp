ec71fbb60116398e43da6f5d312c92d649024139
刘怡豪 <1163122901@qq.com>
2023-07-06 14:51:25 +0800

    add 2178

---
+++ LeetCode/2178_拆分成最多数目的正偶数之和.cpp
@@ -0,0 +1,19 @@
+// 2178_拆分成最多数目的正偶数之和.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<long long> maximumEvenSplit(long long finalSum) {
+        if (finalSum & 1) return {};
+        vector<long long> result;
+        long long temp = 2;
+        while (finalSum > 2 * temp) {
+            result.push_back(temp);
+            finalSum -= temp;
+            temp += 2;
+        }
+        if (finalSum > 0) result.push_back(finalSum);
+        return result;
+    }
+};
