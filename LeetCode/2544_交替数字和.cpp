bca8fd3adeb3b56bafc632f359a098e8943d1491
刘怡豪 <1163122901@qq.com>
2023-07-12 09:36:49 +0800

    add 2544

---
+++ LeetCode/2544_交替数字和.cpp
@@ -0,0 +1,21 @@
+// 2544_½»ÌæÊý×ÖºÍ.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+        vector<int> digits;
+        while (n > 0) {
+            digits.push_back(n % 10);
+            n /= 10;
+        }
+        int sign = 1;
+        int result = 0;
+        for (auto iter = digits.rbegin(); iter != digits.rend(); ++iter) {
+            result += sign * *iter;
+            sign = -sign;
+        }
+        return result;
+    }
+};
