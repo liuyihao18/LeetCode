d2884781bab66e39382e0fdca18ac8ec33afc358
刘怡豪 <1163122901@qq.com>
2023-08-02 09:48:39 +0800

    add 822

---
+++ LeetCode/822_翻转卡片游戏.cpp
@@ -0,0 +1,59 @@
+// 822_·­×ª¿¨Æ¬ÓÎÏ·.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int flipgame(vector<int>& fronts, vector<int>& backs) {
+        int minCard = INT_MAX;
+        for (size_t i = 0; i < fronts.size(); i++) {
+            if (fronts[i] == backs[i]) {
+                continue;
+            }
+            if (fronts[i] < minCard) {
+                int temp = fronts[i];
+                bool check = true;
+                for (size_t j = 0; j < fronts.size(); j++) {
+                    if (j == i) {
+                        continue;
+                    }
+                    int front = fronts[j];
+                    int back = backs[j];
+                    if (front == temp && back == temp) {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check) {
+                    minCard = temp;
+                }
+            }
+            if (backs[i] < minCard) {
+                int temp = backs[i];
+                bool check = true;
+                for (size_t j = 0; j < fronts.size(); j++) {
+                    if (j == i) {
+                        continue;
+                    }
+                    int front = fronts[j];
+                    int back = backs[j];
+                    if (front == temp && back == temp) {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check) {
+                    minCard = temp;
+                }
+            }
+
+        }
+        return minCard == INT_MAX ? 0 : minCard;
+    }
+};
+
+void trimLeftTrailingSpaces(string& input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+        }));
+}
