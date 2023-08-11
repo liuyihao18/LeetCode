7a0bf0af6c198dfca343650d8d9be84d9825c37d
刘怡豪 <liuyihao18@foxmail.com>
2023-08-11 10:05:35 +0800

    add 1572

---
+++ LeetCode/1572_矩阵对角线元素的和.cpp
@@ -0,0 +1,19 @@
+// 1572_矩阵对角线元素的和.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        size_t n = mat.size();
+        int sum = 0;
+        for (size_t i = 0; i < n; i++) {
+            for (size_t j = 0; j < n; j++) {
+                if (i == j || i + j + 1 == n) {
+                    sum += mat[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+};
