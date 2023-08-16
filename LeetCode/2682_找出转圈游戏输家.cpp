ef3104fa90d5ba1fae7a978bd828eccd6d92fe84
刘怡豪 <liuyihao18@foxmail.com>
2023-08-16 11:29:06 +0800

    add 2682

---
+++ LeetCode/2682_找出转圈游戏输家.cpp
@@ -0,0 +1,28 @@
+// 2682_找出转圈游戏输家.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> circularGameLosers(int n, int k) {
+        vector<bool> visited(n, false);
+        int i = 0, epoch = 1;
+        while (true) {
+            if (visited[i]) {
+                break;
+            }
+            else {
+                visited[i] = true;
+            }
+            i = (i + epoch * k) % n;
+            epoch++;
+        }
+        vector<int> answer;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                answer.push_back(i + 1);
+            }
+        }
+        return answer;
+    }
+};
