c5df8ab58467ae2fdbd38a864e739d537af78529
刘怡豪 <liuyihao18@foxmail.com>
2023-08-26 11:29:09 +0800

    add 228

---
+++ LeetCode/228_汇总区间.cpp
@@ -0,0 +1,31 @@
+// 228_»ã×ÜÇø¼ä.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        if (nums.empty()) return {};
+        long long last = nums[0] - 1LL, left = nums[0];
+        vector<string> result;
+        for (auto num : nums) {
+            if (num != last + 1) {
+                if (left == last) {
+                    result.push_back(to_string(left));
+                }
+                else {
+                    result.push_back(to_string(left) + "->" + to_string(last));
+                }
+                left = num;
+            }
+            last = num;
+        }
+        if (left == last) {
+            result.push_back(to_string(left));
+        }
+        else {
+            result.push_back(to_string(left) + "->" + to_string(last));
+        }
+        return result;
+    }
+};
