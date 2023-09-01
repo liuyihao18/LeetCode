5bafe5a8cea7daaf9ea3b34807867f37ab31cd66
刘怡豪 <1163122901@qq.com>
2023-09-01 13:11:25 +0800

    add 2240

---
+++ LeetCode/2240_买钢笔和铅笔的方案数.cpp
@@ -0,0 +1,14 @@
+// 2240_买钢笔和铅笔的方案数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        long long result = 0;
+        for (int i = 0; i * cost1 <= total; i++) {
+            result += (total - i * cost1) / cost2 + 1;
+        }
+        return result;
+    }
+};
