5540c8341a24e3ac84e74288c16c45fc49012a4e
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-09-02 13:52:13 +0800

    add 2511

---
+++ LeetCode/2511_最多可以摧毁的敌人城堡数目.cpp
@@ -0,0 +1,25 @@
+// 2511_最多可以摧毁的敌人城堡数目.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int captureForts(vector<int>& forts) {
+        int pre = -2, post = 2;
+        int maxForts = 0, currForts = 0;
+        for (auto fort : forts) {
+            if (fort != 0) {
+                post = fort;
+                if (pre == 1 && post == -1 || pre == -1 && post == 1) {
+                    maxForts = max(maxForts, currForts);
+                }
+                currForts = 0;
+                pre = fort;
+            }
+            else {
+                currForts++;
+            }
+        }
+        return maxForts;
+    }
+};
