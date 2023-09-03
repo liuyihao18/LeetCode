39ff6805591247a053492d163ddedcb0d0e0ef77
刘怡豪 <1163122901@qq.com>
2023-09-03 13:03:40 +0800

    add 1921

---
+++ LeetCode/1921_消灭怪物的最大数量.cpp
@@ -0,0 +1,27 @@
+// 1921_消灭怪物的最大数量.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        priority_queue<double, vector<double>, greater<double>> q;
+        for (size_t i = 0; i < dist.size(); i++) {
+            q.push(dist[i] * 1.0 / speed[i]);
+        }
+        int time = 0;
+        int eliminate = 0;
+        while (!q.empty()) {
+            auto t = q.top();
+            q.pop();
+            if (time < t) {
+                eliminate++;
+            }
+            else {
+                break;
+            }
+            time++;
+        }
+        return eliminate;
+    }
+};
