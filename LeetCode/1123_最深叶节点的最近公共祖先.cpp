a07f22a444e5a100167d868c3b57bd90e6923651
刘怡豪 <1163122901@qq.com>
2023-09-06 09:38:55 +0800

    add 1123

---
+++ LeetCode/1123_最深叶节点的最近公共祖先.cpp
@@ -0,0 +1,35 @@
+// 1123_最深叶节点的最近公共祖先.cpp
+#include "custom.h"
+ustd
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+    pair<TreeNode*, int> dfs(TreeNode* root) {
+        if (!root) {
+            return { root, -1 };
+        }
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+        if (left.second > right.second) {
+            return { left.first, left.second + 1 };
+        }
+        if (right.second > left.second) {
+            return { right.first, right.second + 1 };
+        }
+        return { root, left.second + 1 };
+    }
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        return dfs(root).first;
+    }
+};
