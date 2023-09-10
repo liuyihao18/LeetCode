ac17f712ad46313e4affc5db7ed9b8d8d8374718
刘怡豪 <1163122901@qq.com>
2023-09-10 14:26:58 +0800

    add 210

---
+++ LeetCode/210_课程表_II.cpp
@@ -0,0 +1,35 @@
+// 210_¿Î³Ì±í_II.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> inDegree(numCourses, 0);
+        vector<vector<int>> edges(numCourses);
+        for (const auto& prerequisite : prerequisites) {
+            int u = prerequisite.back(), v = prerequisite.front();
+            inDegree[v]++;
+            edges[u].push_back(v);
+        }
+        queue<int> q;
+        for (int i = 0; i < inDegree.size(); i++) {
+            if (inDegree[i] == 0) {
+                q.emplace(i);
+            }
+        }
+        vector<int> result;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            result.push_back(u);
+            for (auto v : edges[u]) {
+                inDegree[v]--;
+                if (inDegree[v] == 0) {
+                    q.emplace(v);
+                }
+            }
+        }
+        return result.size() == numCourses ? result : vector<int>();
+    }
+};
