a8e0f665e58f4f23577aca5daaa20b32aea54bfd
刘怡豪 <1163122901@qq.com>
2023-09-13 09:54:07 +0800

    add 2596

---
+++ LeetCode/2596_检查骑士巡视方案.cpp
@@ -0,0 +1,29 @@
+// 2596_检查骑士巡视方案.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool checkValidGrid(vector<vector<int>>& grid) {
+        if (grid[0][0] != 0) {
+            return false;
+        }
+        int n = grid.size();
+        vector<pair<int, int>> route(n * n, {0, 0});
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                route[grid[i][j]] = { i,j };
+            }
+        }
+        for (int i = 0; i + 1 < route.size(); i++) {
+            pair<int, int> pos = route[i];
+            pair<int, int> nextPos = route[i + 1];
+            int xDiff = abs(pos.first - nextPos.first);
+            int yDiff = abs(pos.second - nextPos.second);
+            if (!(xDiff == 1 && yDiff == 2 || xDiff == 2 && yDiff == 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
