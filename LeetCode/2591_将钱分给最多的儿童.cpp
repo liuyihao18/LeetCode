d735cc575614e90c0d2eab33369ab9ec5cb1bfd8
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2023-09-22 13:33:13 +0800

    add 2591

---
+++ LeetCode/2591_将钱分给最多的儿童.cpp
@@ -0,0 +1,14 @@
+// 2591_将钱分给最多的儿童.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int distMoney(int money, int children) {
+        if (money < children) return -1;
+        int temp = children * 8 - money;
+        if (temp == 4) return children - 2;
+        if (temp < 0) return children - 1;
+        return (money - children) / 7;
+    }
+};
