41cb59caea22fca3cbd337641c4b4320dbada5b4
刘怡豪 <mildwind2022@gmail.com>
2024-04-02 14:06:54 +0800

    add 894

---
+++ LeetCode/894_所有可能的真二叉树.cpp
@@ -0,0 +1,36 @@
+// 894_所有可能的真二叉树.cpp
+#include "custom.h"
+ustd
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    vector<TreeNode*> allPossibleFBT(int n) {
+        if (n % 2 == 0) {
+            return {};
+        }
+        if (n == 1) {
+            return { new TreeNode(0) };
+        }
+        vector<TreeNode*> roots;
+        for (int i = 1; i < n; i += 2) {
+            vector<TreeNode*> left = allPossibleFBT(i);
+            vector<TreeNode*> right = allPossibleFBT(n - 1 - i);
+            for (auto l : left) {
+                for (auto r : right) {
+                    roots.push_back(new TreeNode(0, l, r));
+                }
+            }
+        }
+        return roots;
+    }
+};
