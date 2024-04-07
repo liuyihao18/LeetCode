ec229c1751cf815117a1d4165f1fd3ab054e34a9
刘怡豪 <mildwind2022@gmail.com>
2024-04-07 16:51:53 +0800

    add 1600

---
+++ LeetCode/1600_王位继承顺序.cpp
@@ -0,0 +1,44 @@
+// 1600_ÍõÎ»¼Ì³ÐË³Ðò.cpp
+#include "custom.h"
+ustd
+
+class ThroneInheritance {
+    string kingName;
+    map<string, pair<bool, vector<string>>> family;
+public:
+    ThroneInheritance(string kingName) : kingName(kingName) {
+        family[kingName] = { true, {} };
+    }
+
+    void birth(string parentName, string childName) {
+        family[parentName].second.push_back(childName);
+        family[childName] = { true, {} };
+    }
+
+    void death(string name) {
+        family[name].first = false;
+    }
+
+    void getSuccessor(string x, vector<string>& curOrder) {
+        if (family[x].first) {
+            curOrder.push_back(x);
+        }
+        for (const auto& child : family[x].second) {
+            getSuccessor(child, curOrder);
+        }
+    }
+
+    vector<string> getInheritanceOrder() {
+        vector<string> curOrder;
+        getSuccessor(kingName, curOrder);
+        return curOrder;
+    }
+};
+
+/**
+ * Your ThroneInheritance object will be instantiated and called as such:
+ * ThroneInheritance* obj = new ThroneInheritance(kingName);
+ * obj->birth(parentName,childName);
+ * obj->death(name);
+ * vector<string> param_3 = obj->getInheritanceOrder();
+ */
