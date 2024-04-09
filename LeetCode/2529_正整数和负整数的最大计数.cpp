e9394c93aee2e07edb99da9e65fcfb33191d67b5
刘怡豪 <liuyihao18@foxmail.com>
2024-04-09 20:42:46 +0800

    add 2529

---
+++ LeetCode/2529_正整数和负整数的最大计数.cpp
@@ -0,0 +1,19 @@
+// 2529_正整数和负整数的最大计数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int pos = 0, neg = 0;
+        for (auto num : nums) {
+            if (num > 0) {
+                pos++;
+            }
+            else if (num < 0) {
+                neg++;
+            }
+        }
+        return pos > neg ? pos : neg;
+    }
+};
