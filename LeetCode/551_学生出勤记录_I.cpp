d6715136859a77d282c59949a0e3a8c2acf8c34d
刘怡豪 <liuyihao18@foxmail.com>
2024-08-18 10:23:07 +0800

    add 551

---
+++ LeetCode/551_学生出勤记录_I.cpp
@@ -0,0 +1,12 @@
+// 551_学生出勤记录_I.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool checkRecord(string s) {
+        bool flag1 = count_if(s.begin(), s.end(), [](char c) { return c == 'A'; }) < 2;
+        bool flag2 = s.find("LLL") == string::npos;
+        return flag1 && flag2;
+    }
+};
