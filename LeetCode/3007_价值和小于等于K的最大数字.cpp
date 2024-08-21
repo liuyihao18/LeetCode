b41731bc034f3a4e73bdf5ba48025966e2239dd6
刘怡豪 <mildwind2022@gmail.com>
2024-08-21 11:26:12 +0800

    add 3007

---
+++ LeetCode/3007_价值和小于等于K的最大数字.cpp
@@ -0,0 +1,33 @@
+// 3007_价值和小于等于K的最大数字.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    long long getAccumulateValue(long long num, int x) {
+        long long res = 0;
+        // y <= 62
+        for (int y = x; y <= 64 - __builtin_clzll(num); y += x) {
+            long long period = 1LL << y;
+            res += period / 2 * (num / period);
+            if (num % period >= period / 2) {
+                res += num % period - (period / 2 - 1);
+            }
+        }
+        return res;
+    }
+
+public:
+    long long findMaximumNumber(long long k, int x) {
+        long long l = 1, r = (k + 1) << x;
+        while (l < r) {
+            long long m = (l + r + 1) / 2;
+            if (getAccumulateValue(m, x) > k) {
+                r = m - 1;
+            }
+            else {
+                l = m;
+            }
+        }
+        return l;
+    }
+};
