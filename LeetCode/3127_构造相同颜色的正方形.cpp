6b37749266d4fc984dfa6bd96755a79eca32b485
刘怡豪 <liuyihao18@foxmail.com>
2024-08-31 11:01:26 +0800

    add 3127

---
+++ LeetCode/3127_构造相同颜色的正方形.cpp
@@ -0,0 +1,47 @@
+// 3127_构造相同颜色的正方形.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    bool checkSquare(const vector<vector<char>>& grid) {
+        size_t m = grid.size();
+        size_t n = grid[0].size();
+        for (size_t i = 0; i + 1 < m; i++) {
+            for (size_t j = 0; j + 1 < n; j++) {
+                auto temp = { grid[i][j], grid[i + 1][j], grid[i][j + 1], grid[i + 1][j + 1] };
+                if (all_of(temp.begin(), temp.end(),
+                    [&](char c) {
+                        return c == grid[i][j];
+                    }
+                )) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+public:
+    bool canMakeSquare(vector<vector<char>>& grid) {
+        if (checkSquare(grid)) {
+            return true;
+        }
+        size_t m = grid.size();
+        size_t n = grid[0].size();
+        for (size_t i = 0; i < m; i++) {
+            for (size_t j = 0; j < n; j++) {
+                auto temp = grid[i][j];
+                if (grid[i][j] == 'B') {
+                    grid[i][j] = 'W';
+                }
+                else {
+                    grid[i][j] = 'B';
+                }
+                if (checkSquare(grid)) {
+                    return true;
+                }
+                grid[i][j] = temp;
+            }
+        }
+        return false;
+    }
+};
