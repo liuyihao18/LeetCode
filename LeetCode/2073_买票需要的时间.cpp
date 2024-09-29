e3e10ba0e6e3e21e40ea9bbcf26361ac2b454261
刘怡豪 <mildwind2022@gmail.com>
2024-09-29 16:25:57 +0800

    add 2073

---
+++ LeetCode/2073_买票需要的时间.cpp
@@ -0,0 +1,20 @@
+// 2073_买票需要的时间.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int need = tickets[k];
+        int time = 0;
+        for (size_t i = 0; i < tickets.size(); i++) {
+            if (i <= k) {
+                time += min(need, tickets[i]);
+            }
+            else {
+                time += min(need - 1, tickets[i]);
+            }
+        }
+        return time;
+    }
+};
