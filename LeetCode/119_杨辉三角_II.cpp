38793c41d2bd1a52de64c34279b443392c84ec80
刘怡豪 <mildwind2022@gmail.com>
2025-01-28 10:51:54 +0800

    add 119

---
+++ LeetCode/119_杨辉三角_II.cpp
@@ -0,0 +1,29 @@
+// 119_Ñî»ÔÈý½Ç_II.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+private:
+    long long C(long long m, long long n) {
+        if (m == 0) {
+            return 1;
+        }
+        if (n > m / 2) {
+            n = m - n;
+        }
+        long long res = 1;
+        for (long long i = 0; i < n; i++) {
+            res *= (m - i);
+            res /= i + 1;
+        }
+        return res;
+    }
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row(rowIndex + 1, 0);
+        for (int i = 0; i <= rowIndex; i++) {
+            row[i] = C(rowIndex, i);
+        }
+        return row;
+    }
+};
