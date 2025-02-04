e9e1ff3d1cfd5af9fe575d75bc3e98d1a529eea3
刘怡豪 <mildwind2022@gmail.com>
2025-02-04 09:16:48 +0800

    add 922

---
+++ LeetCode/922_按奇偶排序数组_II.cpp
@@ -0,0 +1,24 @@
+// 922_°´ÆæÅ¼ÅÅÐòÊý×é_II.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        size_t i = 0, j = 1, n = nums.size();
+        while (i < n && j < n) {
+            if (nums[i] % 2 == 0) {
+                i += 2;
+                continue;
+            }
+            if (nums[j] % 2 == 1) {
+                j += 2;
+                continue;
+            }
+            swap(nums[i], nums[j]);
+            i += 2;
+            j += 2;
+        }
+        return nums;
+    }
+};
