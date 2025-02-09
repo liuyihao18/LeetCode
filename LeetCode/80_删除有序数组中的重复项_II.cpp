ad4ee41ffebce087adf500d1e502f16a016f34b7
刘怡豪 <mildwind2022@gmail.com>
2025-02-09 10:35:06 +0800

    add 80

---
+++ LeetCode/80_删除有序数组中的重复项_II.cpp
@@ -0,0 +1,16 @@
+// 80_删除有序数组中的重复项_II.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        size_t i = 0;
+        for (int num : nums) {
+            if (i < 2 || num > nums[i - 2]) {
+                nums[i++] = num;
+            }
+        }
+        return i;
+    }
+};
