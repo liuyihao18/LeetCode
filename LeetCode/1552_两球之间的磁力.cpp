f8cffbac130c87e26ebde0f02dc9c176c0c1c3eb
刘怡豪 <mildwind2022@gmail.com>
2025-02-14 10:38:13 +0800

    add 1552

---
+++ LeetCode/1552_两球之间的磁力.cpp
@@ -0,0 +1,36 @@
+// 1552_两球之间的磁力.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(), position.end());
+        int left = 1, right = *position.rbegin();
+        auto check = [&](int force) {
+            int cnt = 1;
+            size_t i = 0, j = 1;
+            while (j < position.size()) {
+                if (position[j] - position[i] >= force) {
+                    cnt++;
+                    i = j;
+                    j++;
+                }
+                else {
+                    j++;
+                }
+            }
+            return cnt >= m;
+            };
+        while (left < right) {  // [left, right)
+            int mid = (left + right) / 2;
+            if (check(mid)) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return left - 1;
+    }
+};
