0434ac8657aa5d3b43ed5d66ebe730b804b424af
刘怡豪 <mildwind2022@gmail.com>
2025-02-15 14:54:16 +0800

    update 1706

--- LeetCode/1706_球会落何处.cpp
+++ LeetCode/1706_球会落何处.cpp
@@ -5,6 +5,30 @@ ustd
 class Solution {
 public:
     vector<int> findBall(vector<vector<int>>& grid) {
+        int m = static_cast<int>(grid.size());
+        int n = static_cast<int>(grid[0].size());
+        vector<int> position(n, 0);
+        iota(position.begin(), position.end(), 0);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (position[j] != -1) {
+                    if (grid[i][position[j]] == 1) {
+                        position[j] = position[j] + 1 == n || grid[i][static_cast<size_t>(position[j] + 1)] == -1 ? -1 : position[j] + 1;
+                    }
+                    else {
+                        position[j] = position[j] == 0 || grid[i][static_cast<size_t>(position[j] - 1)] == 1 ? -1 : position[j] - 1;
+                    }
+                }
+            }
+        }
+        return position;
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<int> findBall(vector<vector<int>>& grid) {
         size_t m = grid.size();
         size_t n = grid.front().size();
         vector<int> result(n, 0);
@@ -38,3 +62,4 @@ public:
         return result;
     }
 };
+*/
