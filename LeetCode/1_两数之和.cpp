aaebd9b1d6682fce35a5c33dd34a3026619c55a8
刘怡豪 <mildwind2022@gmail.com>
2025-02-15 20:37:30 +0800

    update 1

--- LeetCode/1_两数之和.cpp
+++ LeetCode/1_两数之和.cpp
@@ -7,7 +7,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> um;
         for (int i = 0; i < nums.size(); i++) {
-            if (um.count(target - nums[i]) && um[target - nums[i]] != i) {
+            if (um.count(target - nums[i])) {
                 return { i, um[target - nums[i]] };
             }
             um[nums[i]] = i;
