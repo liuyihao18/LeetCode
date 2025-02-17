eb75317d241b20874eb2763625600065cf878202
刘怡豪 <mildwind2022@gmail.com>
2025-02-17 16:05:48 +0800

    滑动窗口

---
+++ LeetCode/239_滑动窗口最大值.cpp
@@ -0,0 +1,52 @@
+// 239_滑动窗口最大值.cpp
+#include "custom.h"
+#include <list>
+ustd
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, size_t k) {
+        list<int> q;
+        for (size_t i = 0; i < k; i++) {
+            while (!q.empty() && q.back() < nums[i]) {
+                q.pop_back();
+            }
+            q.push_back(nums[i]);
+        }
+        vector<int> result;
+        result.push_back(q.front());
+        for (size_t i = k; i < nums.size(); i++) {
+            while (!q.empty() && q.back() < nums[i]) {
+                q.pop_back();
+            }
+            q.push_back(nums[i]);
+            if (q.front() == nums[i - k]) {
+                q.pop_front();
+            }
+            result.push_back(q.front());
+        }
+        return result;
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, size_t k) {
+        map<int, int, greater<>> slidingWindow;
+        for (size_t i = 0; i < k; i++) {
+            ++slidingWindow[nums[i]];
+        }
+        vector<int> result;
+        result.push_back(slidingWindow.begin()->first);
+        for (size_t i = k; i < nums.size(); i++) {
+            ++slidingWindow[nums[i]];
+            if (--slidingWindow[nums[i - k]] == 0) {
+                slidingWindow.erase(nums[i - k]);
+            }
+            result.push_back(slidingWindow.begin()->first);
+        }
+        return result;
+    }
+};
+*/
---
+++ LeetCode/76_最小覆盖子串.cpp
@@ -0,0 +1,40 @@
+// 76_×îÐ¡¸²¸Ç×Ó´®.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    bool greater(const array<int, 128>& cnt_s, const array<int, 128>& cnt_t) {
+        for (size_t i = 0; i < 128; i++) {
+            if (cnt_s[i] < cnt_t[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    string minWindow(string s, string t) {
+        if (s.size() < t.size()) {
+            return "";
+        }
+        array<int, 128> cnt_s = {};
+        array<int, 128> cnt_t = {};
+        for (auto c : t) {
+            cnt_t[c]++;
+        }
+        string result;
+        size_t minLength = s.size() + 1, i = 0, j = 0;
+        while (j < s.size()) {
+            cnt_s[s[j++]]++;
+            if (greater(cnt_s, cnt_t)) {
+                while (i < j && greater(cnt_s, cnt_t)) {
+                    cnt_s[s[i++]]--;
+                }
+                if (j - i + 1 < minLength) {
+                    minLength = j - i + 1;
+                    result = s.substr(i - 1, j - i + 1);
+                }
+            }
+        }
+        return result;
+    }
+};
