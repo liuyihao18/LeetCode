1dcfb5a2362d3c8eaa2447515fed1d23372563cf
刘怡豪 <mildwind2022@gmail.com>
2025-02-18 13:48:11 +0800

    add 2080

---
+++ LeetCode/2080_区间内查询数字的频率.cpp
@@ -0,0 +1,22 @@
+// 2080_区间内查询数字的频率.cpp
+#include "custom.h"
+ustd
+
+constexpr size_t MAX_NUM = 10005;
+
+class RangeFreqQuery {
+    vector<vector<int>> positions;
+public:
+    RangeFreqQuery(vector<int>& arr) : positions(MAX_NUM, vector<int>()) {
+        for (size_t i = 0; i < arr.size(); i++) {
+            positions[arr[i]].push_back(i);
+        }
+    }
+
+    int query(int left, int right, int value) {
+        const vector<int>& position = positions[value];
+        auto leftPos = lower_bound(position.begin(), position.end(), left);
+        auto rightPos = upper_bound(position.begin(), position.end(), right);
+        return distance(leftPos, rightPos);
+    }
+};
