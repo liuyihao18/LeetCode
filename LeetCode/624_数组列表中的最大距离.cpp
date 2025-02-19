be310181cf3dcce08b7ae4e585c5eaf4d6338d00
刘怡豪 <mildwind2022@gmail.com>
2025-02-19 13:25:41 +0800

    add 624

---
+++ LeetCode/624_数组列表中的最大距离.cpp
@@ -0,0 +1,42 @@
+// 624_数组列表中的最大距离.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int maximum1 = INT_MIN, minimum1 = INT_MAX;
+        size_t maximum1_idx = -1, minimum1_idx = -1;
+        size_t n = arrays.size();
+        for (size_t i = 0; i < n; i++) {
+            if (arrays[i].back() > maximum1) {
+                maximum1 = arrays[i].back();
+                maximum1_idx = i;
+            }
+            if (arrays[i].front() < minimum1) {
+                minimum1 = arrays[i].front();
+                minimum1_idx = i;
+            }
+        }
+        int maximum2 = INT_MIN, minimum2 = INT_MAX;
+        for (size_t i = 0; i < n; i++) {
+            if (i != minimum1_idx && arrays[i].back() > maximum2) {
+                maximum2 = arrays[i].back();
+            }
+            if (i != maximum1_idx && arrays[i].front() < minimum2) {
+                minimum2 = arrays[i].front();
+            }
+        }
+        return max(maximum1 - minimum2, maximum2 - minimum1);
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<vector<int>> arrays = {
+        {1, 4},
+        {0, 5}
+    };
+    cout << solution.maxDistance(arrays) << endl;
+    return 0;
+}
