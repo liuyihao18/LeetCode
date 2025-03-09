41042e6ad04ca6112480d21c046c33d538af4acf
刘怡豪 <mildwind2022@gmail.com>
2025-03-09 13:31:35 +0800

    add 2070

---
+++ LeetCode/2070_每一个查询的最大美丽值.cpp
@@ -0,0 +1,30 @@
+// 2070_每一个查询的最大美丽值.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        sort(items.begin(), items.end(), [](const vector<int>& item1, const vector<int>& item2) {
+            return item1[0] < item2[0] || item1[0] == item2[0] && item1[1] > item2[1];
+            });
+        int maximumBeauty = 0;
+        map<int, int> maximumBeauties;
+        for (size_t i = 0; i < items.size(); i++) {
+            int price = items[i][0];
+            maximumBeauty = max(maximumBeauty, items[i][1]);
+            maximumBeauties[price] = maximumBeauty;
+        }
+        vector<int> results;
+        for (auto query : queries) {
+            auto iter = maximumBeauties.upper_bound(query);
+            if (iter == maximumBeauties.begin()) {
+                results.push_back(0);
+                continue;
+            }
+            --iter;
+            results.push_back(iter->second);
+        }
+        return results;
+    }
+};
