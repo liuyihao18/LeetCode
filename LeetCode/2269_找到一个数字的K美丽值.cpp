ba0636a876d01739318a8d6c36e72fe74106bc9c
刘怡豪 <mildwind2022@gmail.com>
2025-03-10 13:24:46 +0800

    add 2269

---
+++ LeetCode/2269_找到一个数字的K美丽值.cpp
@@ -0,0 +1,19 @@
+// 2269_找到一个数字的K美丽值.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int divisorSubstrings(int num, int k) {
+        string numStr = to_string(num);
+        int result = 0;
+        for (size_t i = 0; i + k <= numStr.size(); i++) {
+            string subStr = numStr.substr(i, k);
+            int sub = stoi(subStr);
+            if (sub != 0 && num % sub == 0) {
+                result++;
+            }
+        }
+        return result;
+    }
+};
