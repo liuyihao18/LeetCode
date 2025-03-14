8ae0a76d628e3cdabad7fdf85f78dc87fd0eef46
刘怡豪 <mildwind2022@gmail.com>
2025-03-14 13:36:17 +0800

    add 3340

---
+++ LeetCode/3340_检查平衡字符串.cpp
@@ -0,0 +1,21 @@
+// 3340_¼ì²éÆ½ºâ×Ö·û´®.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int sum = 0;
+        bool add = true;
+        for (auto c : num) {
+            if (add) {
+                sum += c - '0';
+            }
+            else {
+                sum -= c - '0';
+            }
+            add = !add;
+        }
+        return sum == 0;
+    }
+};
