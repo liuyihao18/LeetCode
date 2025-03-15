6265c1fcf075e4921f5e712387207660c01d3eac
刘怡豪 <mildwind2022@gmail.com>
2025-03-15 16:08:38 +0800

    add 3110

---
+++ LeetCode/3110_字符串的分数.cpp
@@ -0,0 +1,14 @@
+// 3110_×Ö·û´®µÄ·ÖÊý.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int score = 0;
+        for (size_t i = 0; i + 1 < s.size(); i++) {
+            score += abs(s[i + 1] - s[i]);
+        }
+        return score;
+    }
+};
