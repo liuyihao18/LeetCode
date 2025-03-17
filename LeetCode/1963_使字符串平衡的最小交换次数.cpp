e90c09c656a731e9f34401fef8592a94588dc59b
刘怡豪 <mildwind2022@gmail.com>
2025-03-17 16:08:27 +0800

    add 1963

---
+++ LeetCode/1963_使字符串平衡的最小交换次数.cpp
@@ -0,0 +1,54 @@
+// 1963_使字符串平衡的最小交换次数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minSwaps(string s) {
+        if (s.empty()) {
+            return -1;
+        }
+        int leftCount = 0;
+        int rightCount = 0;
+        int result = 0;
+        size_t i = 0, j = s.size() - 1;
+        while (i < j) {
+            while (i < j) {
+                if (s[i] == '[') {
+                    leftCount++;
+                }
+                else {
+                    leftCount--;
+                }
+                if (leftCount >= 0) {
+                    i++;
+                }
+                else {
+                    break;
+                }
+            }
+            while (i < j) {
+                if (s[j] == '[') {
+                    rightCount--;
+                }
+                else {
+                    rightCount++;
+                }
+                if (rightCount >= 0) {
+                    j--;
+                }
+                else {
+                    break;
+                }
+            }
+            if (i < j) {
+                result++;
+                i++;
+                j--;
+                leftCount = 1;
+                rightCount = 1;
+            }
+        }
+        return result;
+    }
+};
