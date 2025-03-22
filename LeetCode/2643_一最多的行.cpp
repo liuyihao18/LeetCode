a2fc380013d6956dfd98dc51a61c77db14c8b47c
刘怡豪 <mildwind2022@gmail.com>
2025-03-22 14:39:10 +0800

    add 2643

---
+++ LeetCode/2643_一最多的行.cpp
@@ -0,0 +1,19 @@
+// 2643_一最多的行.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        size_t m = mat.size(), n = mat[0].size();
+        int rowIndex = 0, maxCount = 0;
+        for (int i = 0; i < m; i++) {
+            int count = accumulate(mat[i].begin(), mat[i].end(), 0);
+            if (count > maxCount) {
+                maxCount = count;
+                rowIndex = i;
+            }
+        }
+        return { rowIndex, maxCount };
+    }
+};
