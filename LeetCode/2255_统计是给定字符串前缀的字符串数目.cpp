4bb55b2364916c0602213e786de488f3512e54df
刘怡豪 <mildwind2022@gmail.com>
2025-03-24 19:04:12 +0800

    add 2255

---
+++ LeetCode/2255_统计是给定字符串前缀的字符串数目.cpp
@@ -0,0 +1,17 @@
+// 2255_统计是给定字符串前缀的字符串数目.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int countPrefixes(vector<string>& words, string s) {
+        string_view sv(s);
+        int result = 0;
+        for (const string& word : words) {
+            if (word.size() <= sv.size() && word == sv.substr(0, word.size())) {
+                result++;
+            }
+        }
+        return result;
+    }
+};
