6946d748fc862822ac8aeb16f30654a460f078d6
刘怡豪 <mildwind2022@gmail.com>
2025-03-28 10:07:38 +0800

    add 2716

---
+++ LeetCode/2716_最小化字符串长度.cpp
@@ -0,0 +1,13 @@
+// 2716_最小化字符串长度.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minimizedStringLength(string s) {
+        sort(s.begin(), s.end());
+        auto iter = unique(s.begin(), s.end());
+        return static_cast<int>(iter - s.begin());
+        // return static_cast<int>(set<char>(s.begin(), s.end()).size());
+    }
+};
