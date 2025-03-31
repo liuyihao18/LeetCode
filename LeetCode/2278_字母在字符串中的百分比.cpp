b659722859bcdcf1ddc5e1c4a2f83f8f458cb91f
刘怡豪 <mildwind2022@gmail.com>
2025-03-31 22:12:49 +0800

    add 2278

---
+++ LeetCode/2278_字母在字符串中的百分比.cpp
@@ -0,0 +1,11 @@
+// 2278_字母在字符串中的百分比.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        return static_cast<int>(count(s.begin(), s.end(), letter) * 100 / s.size());
+    }
+};
+
