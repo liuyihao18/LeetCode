c6df8886cccc7836bd9a5f2d975fb54fdf3f68f8
刘怡豪 <mildwind2022@gmail.com>
2025-04-02 13:58:45 +0800

    add 2873

---
+++ LeetCode/2873_有序三元组中的最大值_I.cpp
@@ -0,0 +1,19 @@
+// 2873_有序三元组中的最大值_I.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        size_t n = nums.size();
+        long long result = 0;
+        for (size_t i = 0; i < n; i++) {
+            for (size_t j = i + 1; j < n; j++) {
+                for (size_t k = j + 1; k < n; k++) {
+                    result = max(result, (0LL + nums[i] - nums[j]) * 1LL * nums[k]);
+                }
+            }
+        }
+        return result;
+    }
+};
