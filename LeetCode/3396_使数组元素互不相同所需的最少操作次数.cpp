405b384699b84eedb95ee2142e39381606705672
刘怡豪 <mildwind2022@gmail.com>
2025-04-08 15:41:59 +0800

    add 3396

---
+++ LeetCode/3396_使数组元素互不相同所需的最少操作次数.cpp
@@ -0,0 +1,22 @@
+// 3396_使数组元素互不相同所需的最少操作次数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        size_t n = nums.size();
+        int removeCount = 0;
+        vector<int> counts(105, 0);
+        for (size_t i = n - 1; i != -1; i--) {
+            if (counts[nums[i]] > 0) {
+                removeCount = static_cast<int>(i + 1);
+                break;
+            }
+            else {
+                counts[nums[i]]++;
+            }
+        }
+        return (removeCount + 2) / 3;
+    }
+};
