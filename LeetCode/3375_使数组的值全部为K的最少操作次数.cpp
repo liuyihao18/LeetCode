468fa0fe72dfe6175ac3190c3c8fbd8dd4df5775
刘怡豪 <mildwind2022@gmail.com>
2025-04-09 10:25:22 +0800

    add 3375

---
+++ LeetCode/3375_使数组的值全部为K的最少操作次数.cpp
@@ -0,0 +1,22 @@
+// 3375_使数组的值全部为K的最少操作次数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        set<int> s;
+        for (auto num : nums) {
+            s.insert(num);
+        }
+        if (*s.begin() < k) {
+            return -1;
+        }
+        else if (*s.begin() == k) {
+            return static_cast<int>(s.size() - 1);
+        }
+        else {
+            return static_cast<int>(s.size());
+        }
+    }
+};
