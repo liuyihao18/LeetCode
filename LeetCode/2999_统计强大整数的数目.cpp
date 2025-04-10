166a1ddf915c6e4d72acc2f005c2251e9ff97cdd
刘怡豪 <mildwind2022@gmail.com>
2025-04-10 16:57:04 +0800

    add 2999

---
+++ LeetCode/2999_统计强大整数的数目.cpp
@@ -0,0 +1,38 @@
+// 2999_统计强大整数的数目.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    long long getResult(const string& nums, size_t i, int limit) {
+        if (i == nums.size()) {
+            return 1;
+        }
+        long long result = 1;
+        if (nums[i] - '0' > limit) {
+            result = static_cast<long long>(pow(limit + 1, nums.size() - i));
+        }
+        else {
+            result = static_cast<long long>(nums[i] - '0') * static_cast<long long>(pow(limit + 1, nums.size() - i - 1));
+            result += getResult(nums, i + 1, limit);
+        }
+        return result;
+    }
+
+    long long _numberOfPowerfulInt(long long finish, int limit, string s) {
+        if (finish < stoll(s)) {
+            return 0;
+        }
+        long long base = static_cast<long long>(pow(10, s.size()));
+        long long prefix = finish / base ;
+        long long suffix = finish % base;
+        if (suffix < stoll(s)) {
+            prefix--;
+        }
+        return getResult(to_string(prefix), 0, limit);
+    }
+
+public:
+    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
+        return _numberOfPowerfulInt(finish, limit, s) - _numberOfPowerfulInt(start - 1, limit, s);
+    }
+};
