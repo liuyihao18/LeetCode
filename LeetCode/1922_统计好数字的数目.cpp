522ef2e60646154774aa16879a0a1832b88bb606
刘怡豪 <mildwind2022@gmail.com>
2025-04-13 12:05:49 +0800

    add 1922

---
+++ LeetCode/1922_统计好数字的数目.cpp
@@ -0,0 +1,37 @@
+// 1922_统计好数字的数目.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+private:
+    long long remainder { static_cast<long long>(1e9 + 7) };
+    long long _countGoodNumbers(long long n) {
+        if (n == 1) {
+            return 5LL;
+        }
+        if (n == 2) {
+            return 5LL * 4;
+        }
+        if (n == 3) {
+            return 5LL * 4 * 5;
+        }
+        long long half = n >> 1;
+        long long result = 1;
+        if (half & 1) {
+            long long temp = _countGoodNumbers(half - 1);
+            result *= (temp * temp) % remainder * 20;
+        }
+        else {
+            long long temp = _countGoodNumbers(half);
+            result *= (temp * temp) % remainder;
+        }
+        if (n & 1) {
+            result *= 5LL;
+        }
+        return result % remainder;
+    }
+public:
+    int countGoodNumbers(long long n) {
+        return _countGoodNumbers(n);
+    }
+};
