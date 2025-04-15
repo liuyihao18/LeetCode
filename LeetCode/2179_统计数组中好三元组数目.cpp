73cd3248a11447326bcf6ea73105642475400170
刘怡豪 <mildwind2022@gmail.com>
2025-04-15 13:06:43 +0800

    add 2179

---
+++ LeetCode/2179_统计数组中好三元组数目.cpp
@@ -0,0 +1,58 @@
+// 2179_统计数组中好三元组数目.cpp
+#include "custom.h"
+ustd
+
+class FenwickTree {
+private:
+    vector<int> tree;
+public:
+    FenwickTree(int size) : tree(size + 1, 0) {}
+
+    void update(int index, int delta) {
+        index++;
+        while (index < tree.size()) {
+            tree[index] += delta;
+            index += index & -index;
+        }
+    }
+
+    int query(int index) {
+        index++;
+        int res = 0;
+        while (index > 0) {
+            res += tree[index];
+            index -= index & -index;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    long long goodTriplets(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        vector<int> pos2(n), reversedIndexMapping(n);
+        for (int i = 0; i < n; i++) {
+            pos2[nums2[i]] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            reversedIndexMapping[pos2[nums1[i]]] = i;
+        }
+        FenwickTree tree(n);
+        long long res = 0;
+        for (int value = 0; value < n; value++) {
+            int pos = reversedIndexMapping[value];
+            int left = tree.query(pos);
+            tree.update(pos, 1);
+            int right = (n - 1 - pos) - (value - left);
+            res += (long long)left * right;
+        }
+        return res;
+    }
+};
+/*
+作者：力扣官方题解
+链接：https ://leetcode.cn/problems/count-good-triplets-in-an-array/solutions/3650507/tong-ji-shu-zu-zhong-hao-san-yuan-zu-shu-u13d/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
