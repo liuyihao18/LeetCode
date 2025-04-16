107635a3fba6f5ec9eadc29fa24d4b7006fe12ea
刘怡豪 <mildwind2022@gmail.com>
2025-04-16 13:56:39 +0800

    update 2537

--- LeetCode/2537_统计好子数组的数目.cpp
+++ LeetCode/2537_统计好子数组的数目.cpp
@@ -6,22 +6,15 @@ class Solution {
 public:
     long long countGood(vector<int>& nums, long long k) {
         size_t l = 0, r = 0, n = nums.size();
-        map<int, long long> m;
+        unordered_map<int, long long> m;
         long long result = 0;
         long long count = 0;
         while (r < n) {
-            m[nums[r]]++;
-            count += m[nums[r]] - 1;
+            count += m[nums[r++]]++;
             while (count >= k) {
-                result += n - r;
-                count -= m[nums[l]] - 1;
-                m[nums[l]]--;
-                if (m[nums[l]] == 0) {
-                    m.erase(nums[l]);
-                }
-                l++;
+                result += n - r + 1;
+                count -= --m[nums[l++]];
             }
-            r++;
         }
         return result;
     }
