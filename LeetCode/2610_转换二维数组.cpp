8e5701d33138497107fca6e8af7bf4f30408d25c
刘怡豪 <mildwind2022@gmail.com>
2025-04-17 11:49:49 +0800

    add 2610

---
+++ LeetCode/2610_转换二维数组.cpp
@@ -0,0 +1,28 @@
+// 2610_转换二维数组.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        map<int, size_t> counts;
+        for (auto num : nums) {
+            counts[num]++;
+        }
+        vector<vector<int>> results;
+        while (!counts.empty()) {
+            vector<int> result;
+            for (auto iter = counts.begin(); iter != counts.end();) {
+                result.push_back(iter->first);
+                if (--iter->second == 0) {
+                    iter = counts.erase(iter);
+                }
+                else {
+                    ++iter;
+                }
+            }
+            results.push_back(result);
+        }
+        return results;
+    }
+};
