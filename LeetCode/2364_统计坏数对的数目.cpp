d56e61a1898468c99e61c70637bca4740517a17f
刘怡豪 <mildwind2022@gmail.com>
2025-04-19 16:18:20 +0800

    add 2364

---
+++ LeetCode/2364_统计坏数对的数目.cpp
@@ -0,0 +1,27 @@
+// 2364_统计坏数对的数目.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        // j - i == nums[j] - nums[i] => nums[i] - i == nums[j] - j
+        int n = static_cast<int>(nums.size());
+        for (int i = 0; i < n; i++) {
+            nums[i] -= i;
+        }
+        sort(nums.begin(), nums.end());
+        int i = 0, j = 0;
+        long long result = 0;
+        while (j < n) {
+            while (j < n && nums[j] == nums[i]) {
+                j++;
+            }
+            int temp = j - i;
+            result += 1LL * temp * (temp - 1LL) / 2LL;
+            i = j;
+        }
+        result = 1LL * n * (n - 1LL) / 2LL - result;
+        return result;
+    }
+};
