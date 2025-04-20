e69ce38026763c79d1b3cb252f7e8db4f336fab1
刘怡豪 <mildwind2022@gmail.com>
2025-04-20 15:31:57 +0800

    add 781

---
+++ LeetCode/781_森林中的兔子.cpp
@@ -0,0 +1,18 @@
+// 781_森林中的兔子.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        map<int, int> m;
+        for (auto answer : answers) {
+            m[answer]++;
+        }
+        int rabbits = 0;
+        for (const auto& [k, v] : m) {
+            rabbits += ((v - 1) / (k + 1) + 1) * (k + 1);
+        }
+        return rabbits;
+    }
+};
