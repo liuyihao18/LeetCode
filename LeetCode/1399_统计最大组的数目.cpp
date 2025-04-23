75fa0506387601ee7b579f6b3eb36cb71f1b35d5
刘怡豪 <mildwind2022@gmail.com>
2025-04-23 13:28:14 +0800

    add 1399

---
+++ LeetCode/1399_统计最大组的数目.cpp
@@ -0,0 +1,26 @@
+// 1399_统计最大组的数目.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        map<int, int> counts;
+        for (int i = 1; i <= n; i++) {
+            string s = to_string(i);
+            int digitSum = accumulate(s.begin(), s.end(), 0, [](int sum, char c) { return sum += c - '0'; });
+            counts[digitSum]++;
+        }
+        int largest = 0, count = 0;
+        for (const auto& [_, v] : counts) {
+            if (v > largest) {
+                largest = v;
+                count = 1;
+            }
+            else if (v == largest) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
