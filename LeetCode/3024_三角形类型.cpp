38ca4b5b2bbe59996a9d31b3023f4cad535750c7
刘怡豪 <mildwind2022@gmail.com>
2025-05-19 13:48:09 +0800

    add 3024

---
+++ LeetCode/3024_三角形类型.cpp
@@ -0,0 +1,22 @@
+// 3024_三角形类型.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            return "none";
+        }
+        if (a == b && b == c) {
+            return "equilateral";
+        }
+        if (a == b || a == c || b == c) {
+            return "isosceles";
+        }
+        return "scalene";
+    }
+};
