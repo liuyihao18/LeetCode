3e9eb2a4a36766499ab151f451a78d7027940a29
刘怡豪 <mildwind2022@gmail.com>
2025-05-20 15:58:56 +0800

    add 3355

---
+++ LeetCode/3355_零数组变换_I.cpp
@@ -0,0 +1,29 @@
+// 3355_ÁãÊý×é±ä»»_I.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        size_t n = nums.size();
+        vector<int> diff(n);
+        for (const vector<int>& query : queries) {
+            int l = query[0];
+            int r = query[1];
+            diff[r]++;
+            if (l - 1 >= 0) {
+                diff[static_cast<size_t>(l - 1)]--;
+            }
+        }
+        bool flag = true;
+        int sum = 0;
+        for (size_t i = n - 1; i != -1; i--) {
+            sum += diff[i];
+            if (sum < nums[i]) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+};
