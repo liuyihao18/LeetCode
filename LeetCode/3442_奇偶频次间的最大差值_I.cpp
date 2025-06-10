b05f75c3fa7890e1e5e5156d31befa7f2fe8a0ed
刘怡豪 <mildwind2022@gmail.com>
2025-06-10 13:25:29 +0800

    add 3442

---
+++ LeetCode/3442_奇偶频次间的最大差值_I.cpp
@@ -0,0 +1,27 @@
+// 3442_奇偶频次间的最大差值_I.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maxDifference(string s) {
+        vector<int> counts(26, 0);
+        for (char c : s) {
+            counts[static_cast<size_t>(c - 'a')]++;
+        }
+        int odd_count_max = 0;
+        int even_count_min = 1000;
+        for (int count : counts) {
+            if (count == 0) {
+                continue;
+            }
+            if (count & 1) {
+                odd_count_max = max(odd_count_max, count);
+            }
+            else {
+                even_count_min = min(even_count_min, count);
+            }
+        }
+        return odd_count_max - even_count_min;
+    }
+};
