4b8f8e1fe97e9247faf3aa9be3cb9480b0b76b5a
刘怡豪 <mildwind2022@gmail.com>
2025-06-23 21:49:41 +0800

    update 386

--- LeetCode/386_字典序排数.cpp
+++ LeetCode/386_字典序排数.cpp
@@ -3,6 +3,31 @@
 ustd
 
 class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> result;
+        stack<int> st;
+        for (int i = 9; i >= 1; i--) {
+            if (i <= n) {
+                st.push(i);
+            }
+        }
+        while (!st.empty()) {
+            int curr = st.top();
+            st.pop();
+                result.push_back(curr);
+                for (int i = 9; i >= 0; i--) {
+                    if (curr * 10 + i <= n) {
+                        st.push(curr * 10 + i);
+                    }
+                }
+        }
+        return result;
+    }
+};
+
+/*
+class Solution {
     void dfs(int num, int n, vector<int>& result){
         for (int i = 0; i < 10; i++) {
             int temp = num * 10 + i;
@@ -30,3 +55,4 @@ public:
         return result;
     }
 };
+*/
