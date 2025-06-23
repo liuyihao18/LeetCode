3fa29b29d11a0d342b633077711064a2b3ebafa6
刘怡豪 <mildwind2022@gmail.com>
2025-06-23 22:14:37 +0800

    add 3403

---
+++ LeetCode/3403_从盒子中找出字典序最大的字符串_I.cpp
@@ -0,0 +1,25 @@
+// 3403_从盒子中找出字典序最大的字符串_I.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    size_t find(const string_view& word) {
+        size_t index = max_element(word.begin(), word.end()) - word.begin();
+        map<string_view, size_t> subStrings;
+        for (size_t i = 0; i < word.size(); i++) {
+            if (word[i] == word[index]) {
+                subStrings[word.substr(i)] = i;
+            }
+        }
+        return subStrings.rbegin()->second;
+    }
+public:
+    string answerString(string word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
+        }
+        size_t i = find(word);
+        size_t j = word.size() - (numFriends - i);
+        return word.substr(i, j - i + 1);
+    }
+};
