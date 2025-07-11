f1d5b0a0194c2561e0718e98f78fa7fd6182272b
刘怡豪 <mildwind2022@gmail.com>
2025-07-11 09:57:33 +0800

    add 3169

---
+++ LeetCode/3169_无需开会的工作日.cpp
@@ -0,0 +1,25 @@
+// 3169_无需开会的工作日.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+		size_t n = meetings.size();
+        ranges::sort(meetings, 
+            [](const vector<int>& meeting1, const vector<int>& meeting2) {
+			    return meeting1[0] < meeting2[0];
+            }
+        );
+        int res = meetings[0][0] - 1;
+		int right = meetings[0][1];
+        for (size_t i = 1; i < n; i++) {
+            if (meetings[i][0] - right - 1 > 0) {
+                res += meetings[i][0] - right - 1;
+            }
+			right = max(right, meetings[i][1]);
+        }
+		res += days - right;
+        return res;
+    }
+};
