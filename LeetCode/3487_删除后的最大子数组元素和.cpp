799eec7dad8bcfcb7f98133c290a89b24d7d70d1
刘怡豪 <mildwind2022@gmail.com>
2025-07-25 21:39:54 +0800

    add 3487

---
+++ LeetCode/3487_删除后的最大子数组元素和.cpp
@@ -0,0 +1,23 @@
+// 3487_删除后的最大子数组元素和.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        unordered_set<int> us;
+        int maximum = INT_MIN;
+        for (int num : nums) {
+            if (num > 0) {
+                us.insert(num);
+            }
+            maximum = max(maximum, num);
+        }
+        if (us.empty()) {
+            return maximum;
+        }
+        else {
+            return accumulate(us.begin(), us.end(), 0);
+        }
+    }
+};
