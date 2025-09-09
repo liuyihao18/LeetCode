095bce443f0086646a8dfef488e97a67ce5c4064
刘怡豪 <mildwind2022@gmail.com>
2025-09-09 13:36:22 +0800

    add 1304

---
+++ LeetCode/1304_和为零的N个不同整数.cpp
@@ -0,0 +1,19 @@
+// 1304_和为零的N个不同整数.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> results;
+        if (n & 1) {
+            results.push_back(0);
+            n--;
+        }
+        for (int i = 1; i <= n / 2; i++) {
+            results.push_back(i);
+            results.push_back(-i);
+        }
+        return results;
+    }
+};
