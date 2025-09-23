87a40e8a78a0caf6ac557185b6177d874ac00685
刘怡豪 <mildwind2022@gmail.com>
2025-09-23 20:30:36 +0800

    add 165

---
+++ LeetCode/165_比较版本号.cpp
@@ -0,0 +1,41 @@
+// 165_±È½Ï°æ±¾ºÅ.cpp
+#include "custom.h"
+ustd
+
+class Solution {
+    vector<int> split(const string& version) {
+        vector<int> _version;
+        string num;
+        for (char c : version) {
+            if (c == '.') {
+                _version.push_back(stoi(num));
+                num.clear();
+            }
+            else {
+                num += c;
+            }
+        }
+        _version.push_back(stoi(num));
+        return _version;
+    }
+public:
+    int compareVersion(string version1, string version2) {
+        vector<int> _version1 = split(version1);
+        vector<int> _version2 = split(version2);
+        while (_version1.size() < _version2.size()) {
+            _version1.push_back(0);
+        }
+        while (_version1.size() > _version2.size()) {
+            _version2.push_back(0);
+        }
+        if (_version1 < _version2) {
+            return -1;
+        }
+        else if (_version1 > _version2) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+};
