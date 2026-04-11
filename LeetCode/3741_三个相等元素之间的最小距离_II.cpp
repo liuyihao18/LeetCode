957b9f00a908ed9d066de9d72f588ceb0b05357f
刘怡豪 <mildwind2022@gmail.com>
2026-04-11 15:37:39 +0800

    add 3741

---
+++ LeetCode/3741_三个相等元素之间的最小距离_II.cpp
@@ -0,0 +1,26 @@
+// 3741_三个相等元素之间的最小距离_II.cpp
+#include "stdafx.h"
+
+#include <ranges>
+ustd
+
+class Solution {
+public:
+    int minimumDistance(const vector<int>& nums) {
+        unordered_map<int, vector<int>> um;
+        for (int i = 0, n = static_cast<int>(nums.size()); i < n; i++)
+        {
+            um[nums[i]].push_back(i);
+        }
+        int result = INT_MAX;
+        for (auto& value : um | views::values)
+        {
+            for (int i = 0, n = static_cast<int>(value.size()); i + 2 < n; i++)
+            {
+                const int distance = (value[i + 2] - value[i]) * 2;
+                result = min(result, distance);
+            }
+        }
+        return result == INT_MAX ? -1 : result;
+    }
+};
