70977764c495c56f2ba57faa427d820f3ce21db8
刘怡豪 <mildwind2022@gmail.com>
2026-05-24 21:05:28 +0800

    add 1752

--- LeetCode/1752_检查数组是否经排序和轮转得到.cpp
+++ LeetCode/1752_检查数组是否经排序和轮转得到.cpp
@@ -4,6 +4,40 @@ ustd
 
 class Solution {
 public:
+	bool check(const vector<int>& nums) 	{
+		const int n = static_cast<int>(nums.size());
+		const int minIndex = static_cast<int>(ranges::min_element(nums) - nums.begin());
+		// 往回找，看看最小元素出现了几次
+		int count = 1;
+		for (int i = 1; i < n; i++) 
+		{
+			if (const int j = (minIndex - i + n) % n;
+				nums[j] == nums[minIndex]) 
+			{
+				count++;
+			}
+			else
+			{
+				break;
+			}
+		}
+		// 排除头尾接壤的最小元素
+		for (int i = 0; i + count < n; i++)
+		{
+			const int i1 = (minIndex + i) % n;
+			const int i2 = (minIndex + i + 1) % n;
+			if (nums[i1] > nums[i2])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+/*
+class Solution {
+public:
     bool check(vector<int>& nums) {
         vector<int> origin = nums;
         sort(origin.begin(), origin.end());
@@ -21,3 +55,4 @@ public:
         return false;
     }
 };
+*/
