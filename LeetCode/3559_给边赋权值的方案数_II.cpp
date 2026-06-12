19392c49404681fa46c026d7ab264af0fddace9e
刘怡豪 <mildwind2022@gmail.com>
2026-06-12 10:47:41 +0800

    update 3559

--- LeetCode/3559_给边赋权值的方案数_II.cpp
+++ LeetCode/3559_给边赋权值的方案数_II.cpp
@@ -22,30 +22,50 @@ class Solution {
             return (half * half) % module;
         }
     }
-    static void computeAncestorsAndDepth(const vector<vector<int>>& graph, vector<int>& parents, vector<int>& depths) {
-        parents[0] = 0;
-        stack<pair<int, int>> st;
-        st.emplace(0, 0);
-        while (!st.empty()) {
-            const auto [u, depth] = st.top();
-            st.pop();
-            depths[u] = depth;
-            for (const int v : graph[u]) {
-                parents[v] = u;
-                st.emplace(v, depth + 1);
+    static void computeParentsAndDepth(
+        const int u, const vector<vector<int>>& graph,
+        vector<vector<int>>& parents, vector<int>& depth,
+        vector<int>& tempPath, vector<bool>& visited) 
+    {
+        visited[u] = true;
+        // 倍增祖先
+        int pIndex = static_cast<int>(tempPath.size() - 1);
+        int index = 0;
+        while (pIndex >= 0) {
+            parents[u].push_back(tempPath[pIndex]);
+            pIndex -= 1 << index;
+            index++;
+        }
+        // 计算深度
+        if (parents[u].size() == 0) {
+            depth[u] = 0;
+        }
+        else {
+            depth[u] = depth[parents[u][0]] + 1;
+        }
+        // DFS
+        for (const int v : graph[u]) {
+            if (visited[v]) {
+                continue;
             }
+            tempPath.push_back(u);
+            computeParentsAndDepth(v, graph, parents, depth, tempPath, visited);
+            tempPath.pop_back();
         }
     }
-    static int findLowestCommonAncestor(const vector<int>& parents, const vector<int>& depth, int node1, int node2) {
+    static int findLowestCommonAncestor(const vector<vector<int>>& parents, const vector<int>& depth, int node1, int node2) {
+        // 快速跳跃对齐高度
         while (depth[node1] > depth[node2]) {
-            node1 = parents[node1];
+            const int jump = static_cast<int>(log2(depth[node1] - depth[node2]));
+            node1 = parents[node1][jump];
         }
         while (depth[node1] < depth[node2]) {
-            node2 = parents[node2];
+            const int jump = static_cast<int>(log2(depth[node2] - depth[node1]));
+            node2 = parents[node2][jump];
         }
         while (node1 != node2) {
-            node1 = parents[node1];
-            node2 = parents[node2];
+            node1 = parents[node1][0];
+            node2 = parents[node2][0];
         }
         return node1;
     }
@@ -57,11 +77,14 @@ public:
             const int u = edge[0] - 1;
             const int v = edge[1] - 1;
             graph[u].push_back(v);
+            graph[v].push_back(u);
         }
         
-        vector<int> parents(n);
+        vector<vector<int>> parents(n);
         vector<int> depth(n);
-        computeAncestorsAndDepth(graph, parents, depth);
+        vector<int> tempPath;
+        vector<bool> visited(n);
+        computeParentsAndDepth(0, graph, parents, depth, tempPath, visited);
         
         vector<int> results;
         for (const vector<int>& query : queries) {
@@ -75,3 +98,20 @@ public:
         return results;
     }
 };
+
+/*
+int main() {
+    vector<vector<int>> edges = {
+        {3,2},{2,1}
+    };
+    vector<vector<int>> queries = {
+        {3,1}
+    };
+    vector<int> results = Solution().assignEdgeWeights(edges, queries);
+    for (const int result : results) {
+        cout << result << " ";
+    }
+    cout << endl;
+    return 0;
+}
+*/
