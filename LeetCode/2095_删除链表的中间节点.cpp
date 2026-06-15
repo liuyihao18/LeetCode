240813ccff15bef761392ba74dcccb3007ffc9d6
刘怡豪 <mildwind2022@gmail.com>
2026-06-15 20:32:13 +0800

    add 2095

---
+++ LeetCode/2095_删除链表的中间节点.cpp
@@ -0,0 +1,27 @@
+// 2095_删除链表的中间节点.cpp
+#include "stdafx.h"
+ustd
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (head->next == nullptr) return nullptr;
+        ListNode* fast = head->next->next;
+        ListNode* slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        slow->next = slow->next->next;
+        return head;
+    }
+};
