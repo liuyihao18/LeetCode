5c57235bca3eaa68526d1915b85e808f4a451760
刘怡豪 <mildwind2022@gmail.com>
2026-06-16 21:48:23 +0800

    add 3612

---
+++ LeetCode/3612_用特殊操作处理字符串_I.cpp
@@ -0,0 +1,30 @@
+// 3612_用特殊操作处理字符串_I.cpp
+#include "stdafx.h"
+ustd
+
+class Solution {
+public:
+    string processStr(string_view s) {
+        string result;
+        for (char c : s) {
+            switch (c) {
+            case '*':
+                if (!result.empty()) {
+                    result.pop_back();
+                }
+                break;
+            case '#':
+                result = result + result;
+                break;
+            case '%':
+                ranges::reverse(result);
+                break;
+            default:
+                if (isalpha(c) && islower(c)) {
+                    result.push_back(c);
+                }
+            }
+        }
+        return result;
+    }
+};
